4d47bc9b938eea9a9fcadc334ff55f2970581727
Kristjan Nielsen <kristjan.nielsen@arup.com>
2022-01-27 15:55:15 +0100

    updated icon

--- GhSA/Components/0_Model/GetAnalysis.cs
+++ GhSA/Components/0_Model/GetAnalysis.cs
@@ -30,7 +30,7 @@ namespace GhSA.Components
         { this.Hidden = true; } // sets the initial state of the component to hidden
         public override GH_Exposure Exposure => GH_Exposure.secondary | GH_Exposure.obscure;
 
-        protected override System.Drawing.Bitmap Icon => GhSA.Properties.Resources.GetAnalysis;
+        protected override System.Drawing.Bitmap Icon => GhSA.Properties.Resources.GetAnalysisTask;
         #endregion
 
         #region Custom UI
