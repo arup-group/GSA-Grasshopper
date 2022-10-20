5ccc551adbd822a7b6144bfdc93126fe8c8917ec
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-10-20 14:52:09 +0200

    test

--- IntegrationTests/Parameters/1_Properties/EditBool6Test.cs
+++ IntegrationTests/Parameters/1_Properties/EditBool6Test.cs
@@ -23,22 +23,22 @@ namespace IntegrationTests.Parameters
       return io.Document;
     }
 
-    [Theory]
-    [InlineData("X", false)]
-    [InlineData("Y", true)]
-    [InlineData("Z", false)]
-    [InlineData("XX", true)]
-    [InlineData("YY", false)]
-    [InlineData("ZZ", true)]
-    public void OutputTest(string groupIdentifier, bool expected)
-    {
-      GH_Document doc = Document();
-      GH_Param<GH_Boolean> param = Helper.FindComponentInDocumentByGroup<GH_Boolean>(doc, groupIdentifier);
-      Assert.NotNull(param);
-      param.CollectData();
-      GH_Boolean output = (GH_Boolean)param.VolatileData.get_Branch(0)[0];
-      Assert.Equal(expected, output.Value);
-    }
+    //[Theory]
+    //[InlineData("X", false)]
+    //[InlineData("Y", true)]
+    //[InlineData("Z", false)]
+    //[InlineData("XX", true)]
+    //[InlineData("YY", false)]
+    //[InlineData("ZZ", true)]
+    //public void OutputTest(string groupIdentifier, bool expected)
+    //{
+    //  GH_Document doc = Document();
+    //  GH_Param<GH_Boolean> param = Helper.FindComponentInDocumentByGroup<GH_Boolean>(doc, groupIdentifier);
+    //  Assert.NotNull(param);
+    //  param.CollectData();
+    //  GH_Boolean output = (GH_Boolean)param.VolatileData.get_Branch(0)[0];
+    //  Assert.Equal(expected, output.Value);
+    //}
 
     [Fact]
     public void NoRuntimeErrorTest()
