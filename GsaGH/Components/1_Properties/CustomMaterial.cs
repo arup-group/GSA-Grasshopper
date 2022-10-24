f675c9f7fe1dd2a695fe2f073c0ada43edaad531
Kristjan Nielsen <Kristjan.Nielsen@arup.com>
2022-10-24 18:15:42 +0200

    fix character in inherinting

--- GsaGH/Components/1_Properties/CustomMaterial.cs
+++ GsaGH/Components/1_Properties/CustomMaterial.cs
@@ -18,7 +18,7 @@ namespace GsaGH.Components
   /// <summary>
   /// Component to create a new Material
   /// </summary>
-  public class CustomMaterial : GH_OasysDropDownComponent;
+  public class CustomMaterial : GH_OasysDropDownComponent
   {
     #region Name and Ribbon Layout
     public override Guid ComponentGuid => new Guid("83bfce91-9204-4fe4-b81d-0036babf0c6d");
