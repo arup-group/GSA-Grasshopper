a5e0208dd613c0290b2d50aa27902007377d9b8a
Kristjan Nielsen <Kristjan.Nielsen@arup.com>
2022-10-27 16:34:27 +0200

    example 2 test

---
+++ IntegrationTests/Tests/GsaGH_Example2_Reciprocal.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using Xunit;
+
+namespace IntegrationTests.ExampleFiles
+{
+  [Collection("GrasshopperFixture collection")]
+  public class Example2_Reciprocal_Test
+  {
+    public static GH_Document Document()
+    {
+      Type thisClass = MethodBase.GetCurrentMethod().DeclaringType;
+      string fileName = "GsaGH_" + thisClass.Name + ".gh";
+      fileName = fileName.Replace(thisClass.Namespace, string.Empty).Replace("_Test", string.Empty);
+
+      string solutiondir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.FullName;
+      string path = Path.Combine(solutiondir, "ExampleFiles");
+
+      return Helper.CreateDocument(Path.Combine(path, fileName));
+    }
+
+    [Fact]
+    public void MaxMyyAssert()
+    {
+      IGH_Param param = Helper.FindParameter(Document(), "MaxMyy");
+      Assert.NotNull(param);
+      param.CollectData();
+      GH_Number output = (GH_Number)param.VolatileData.get_Branch(0)[0];
+      Assert.Equal(-157.193672, output.Value, 6);
+    }
+
+    [Fact]
+    public void SumLoadForceAssert()
+    {
+      IGH_Param param = Helper.FindParameter(Document(), "SumLoadForce");
+      Assert.NotNull(param);
+      param.CollectData();
+      GH_Boolean output = (GH_Boolean)param.VolatileData.get_Branch(0)[0];
+      Assert.True(output.Value);
+    }
+
+    [Fact]
+    public void NoRuntimeErrorsTest()
+    {
+      Helper.TestNoRuntimeMessagesInDocument(Document(), GH_RuntimeMessageLevel.Error);
+    }
+  }
+}
