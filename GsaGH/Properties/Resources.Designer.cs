e8d302a7cb1303275f80430fb9f7d72d8a2ee610
Kristjan Nielsen <Kristjan.Nielsen@arup.com>
2022-11-15 18:45:47 +0100

    add section alignment component

--- GsaGH/Properties/Resources.Designer.cs
+++ GsaGH/Properties/Resources.Designer.cs
@@ -1175,6 +1175,18 @@ namespace GsaGH.Properties {
     /// <summary>
     ///   Looks up a localized resource of type System.Drawing.Bitmap.
     /// </summary>
+    public static System.Drawing.Bitmap SectionAlignment
+    {
+      get
+      {
+        object obj = ResourceManager.GetObject("SectionAlignment", resourceCulture);
+        return ((System.Drawing.Bitmap)(obj));
+      }
+    }
+
+    /// <summary>
+    ///   Looks up a localized resource of type System.Drawing.Bitmap.
+    /// </summary>
     public static System.Drawing.Bitmap SelectResult {
             get {
                 object obj = ResourceManager.GetObject("SelectResult", resourceCulture);
