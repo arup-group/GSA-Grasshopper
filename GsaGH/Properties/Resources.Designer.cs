7d6cc91f951f5dee6c8441593cc01df522946860
Kristjan Nielsen <Kristjan.Nielsen@arup.com>
2022-11-15 14:32:35 +0100

    add get section dimensions component

--- GsaGH/Properties/Resources.Designer.cs
+++ GsaGH/Properties/Resources.Designer.cs
@@ -1159,11 +1159,23 @@ namespace GsaGH.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        public static System.Drawing.Bitmap SelectResult {
+
+    /// <summary>
+    ///   Looks up a localized resource of type System.Drawing.Bitmap.
+    /// </summary>
+    public static System.Drawing.Bitmap SectionDimensions
+    {
+      get
+      {
+        object obj = ResourceManager.GetObject("SectionDimensions", resourceCulture);
+        return ((System.Drawing.Bitmap)(obj));
+      }
+    }
+
+    /// <summary>
+    ///   Looks up a localized resource of type System.Drawing.Bitmap.
+    /// </summary>
+    public static System.Drawing.Bitmap SelectResult {
             get {
                 object obj = ResourceManager.GetObject("SelectResult", resourceCulture);
                 return ((System.Drawing.Bitmap)(obj));
