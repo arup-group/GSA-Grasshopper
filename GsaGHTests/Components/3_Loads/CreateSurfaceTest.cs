e9a3805766df7abc5cb5d4c2d98701af81c24318
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-11-21 11:28:57 +0100

    tmp

---
+++ GsaGHTests/Components/3_Loads/CreateSurfaceTest.cs
@@ -0,0 +1,31 @@
+﻿using GsaGH.Components;
+using GsaGH.Parameters;
+using GsaGHTests.Helpers;
+using OasysUnits;
+using OasysUnits.Units;
+using Xunit;
+
+namespace ComponentsTest
+{
+  //[Collection("GrasshopperFixture collection")]
+  //public class GridPlaneSurfaceTests
+  //{
+  //  [Fact]
+  //  public void CreateGridPlaneSurfaceTest()
+  //  {
+  //    var comp = new CreateGridPlane();
+  //    comp.CreateAttributes();
+
+  //    ComponentTestHelper.SetInput(comp, 42, 1);
+  //    ComponentTestHelper.SetInput(comp, 10, 2);
+  //    ComponentTestHelper.SetInput(comp, "test", 3);
+
+  //    GsaGridPlaneSurfaceGoo output = (GsaGridPlaneSurfaceGoo)ComponentTestHelper.GetOutput(comp);
+
+  //    GsaGridPlaneSurface gridPlane = null;
+  //    output.CastTo(ref gridPlane);
+
+  //    Assert.Equal(42, gridPlane.GridPlaneID);
+  //  }
+  //}
+}
---
+++ IntegrationTests/Components/GridPlaneSurfaceTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+using GsaGH.Parameters;
+using Xunit;
+
+namespace IntegrationTests.Components
+{
+  [Collection("GrasshopperFixture collection")]
+  public class GridPlaneSurfaceTests
+  {
+    public static GH_Document Document()
+    {
+      Type thisClass = MethodBase.GetCurrentMethod().DeclaringType;
+      string fileName = thisClass.Name + ".gh";
+      fileName = fileName.Replace(thisClass.Namespace, string.Empty).Replace("Tests", string.Empty);
+
+      string solutiondir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.FullName;
+      string path = Path.Combine(new string[] { solutiondir, "ExampleFiles", "Components"});
+      
+      return Helper.CreateDocument(Path.Combine(path, fileName));
+    }
+
+    [Fact]
+    public void GridPlaneSurfaceTest()
+    {
+      GH_Document doc = Document();
+      IGH_Param param = Helper.FindParameter(doc, "gps");
+      GsaGridPlaneSurfaceGoo output = (GsaGridPlaneSurfaceGoo)param.VolatileData.get_Branch(0)[0];
+      GsaGridPlaneSurface gps = output.Value;
+      Assert.Equal(42, gps.GridSurfaceID);
+    }
+  }
+}
