e4eac1a06d8ffd8bf35272c2866a78b5dd45b3a4
Tilman Reinhardt <tilman.reinhardt@arup.com>
2022-11-30 11:00:22 +0100

    tests

--- GsaGHTests/Helper/MappingsTests.cs
+++ GsaGHTests/Helper/MappingsTests.cs
@@ -9,143 +9,143 @@ namespace GsaGHTests.Helpers
   public class MappingsTests
   {
     [Theory]
-    [InlineData("Linear", AnalysisOrder.LINEAR)]
-    [InlineData("Quadratic", AnalysisOrder.QUADRATIC)]
-    [InlineData("Rigid Diaphragm", AnalysisOrder.RIGID_DIAPHRAGM)]
-    [InlineData("rIgId diAphrAgm", AnalysisOrder.RIGID_DIAPHRAGM)]
-    public void GetAnalysisOrderTest(string input, AnalysisOrder expected)
+    [InlineData("Linear", 0)]
+    [InlineData("Quadratic", 1)]
+    [InlineData("Rigid Diaphragm", 2)]
+    [InlineData("rIgId diAphrAgm", 2)]
+    public void GetAnalysisOrderTest(string input, int expected)
     {
       // Act
       AnalysisOrder actual = Mappings.GetAnalysisOrder(input);
 
       // Assert
-      Assert.Equal(expected, actual);
+      Assert.Equal(expected, (int)actual);
     }
 
     [Theory]
-    [InlineData("New", ElementType.NEW)]
-    [InlineData(" New", ElementType.NEW)]
-    [InlineData("Undefined", ElementType.UNDEF)]
-    [InlineData(" Undefined  ", ElementType.UNDEF)]
-    [InlineData("Bar", ElementType.BAR)]
-    [InlineData("bar", ElementType.BAR)]
-    [InlineData("Beam", ElementType.BEAM)]
-    [InlineData("bEAM", ElementType.BEAM)]
-    [InlineData("Spring", ElementType.SPRING)]
-    [InlineData("SPRING", ElementType.SPRING)]
-    [InlineData("Quad-4", ElementType.QUAD4)]
-    [InlineData("qUaD-4", ElementType.QUAD4)]
-    [InlineData("Quad-8", ElementType.QUAD8)]
-    [InlineData("Quad8", ElementType.QUAD8)]
-    [InlineData("Tri-3", ElementType.TRI3)]
-    [InlineData("tri3", ElementType.TRI3)]
-    [InlineData("Tri-6", ElementType.TRI6)]
-    [InlineData("TRI6", ElementType.TRI6)]
-    [InlineData("Link", ElementType.LINK)]
-    [InlineData("Cable", ElementType.CABLE)]
-    [InlineData("Brick-8", ElementType.BRICK8)]
-    [InlineData("Wedge-6", ElementType.WEDGE6)]
-    [InlineData("Tetra-4", ElementType.TETRA4)]
-    [InlineData("Spacer", ElementType.SPACER)]
-    [InlineData("Strut", ElementType.STRUT)]
-    [InlineData("Tie", ElementType.TIE)]
-    [InlineData("Rod", ElementType.ROD)]
-    [InlineData("Damper", ElementType.DAMPER)]
-    [InlineData("Pyramid-5", ElementType.PYRAMID5)]
-    [InlineData("Last Type", ElementType.LAST_TYPE)]
-    [InlineData("1D", ElementType.ONE_D)]
-    [InlineData("2D", ElementType.TWO_D)]
-    [InlineData("3D", ElementType.THREE_D)]
-    [InlineData("1D Section", ElementType.ONE_D_SECT)]
-    [InlineData("2D Finite Element", ElementType.TWO_D_FE)]
-    [InlineData("2d FinitEelement", ElementType.TWO_D_FE)]
-    [InlineData("2D Load", ElementType.TWO_D_LOAD)]
-    [InlineData("2dLoad", ElementType.TWO_D_LOAD)]
-    public void GetElementTypeTest(string input, ElementType expected)
+    [InlineData("New", -1)]
+    [InlineData(" New", -1)]
+    [InlineData("Undefined", 0)]
+    [InlineData(" Undefined  ", 0)]
+    [InlineData("Bar", 1)]
+    [InlineData("bar", 1)]
+    [InlineData("Beam", 2)]
+    [InlineData("bEAM", 2)]
+    [InlineData("Spring", 3)]
+    [InlineData("SPRING", 3)]
+    [InlineData("Quad-4", 5)]
+    [InlineData("qUaD-4", 5)]
+    [InlineData("Quad-8", 6)]
+    [InlineData("Quad8", 6)]
+    [InlineData("Tri-3", 7)]
+    [InlineData("tri3", 7)]
+    [InlineData("Tri-6", 8)]
+    [InlineData("TRI6", 8)]
+    [InlineData("Link", 9)]
+    [InlineData("Cable", 10)]
+    [InlineData("Brick-8",12)]
+    [InlineData("Wedge-6", 14)]
+    [InlineData("Tetra-4", 16)]
+    [InlineData("Spacer", 19)]
+    [InlineData("Strut", 20)]
+    [InlineData("Tie", 21)]
+    [InlineData("Rod", 23)]
+    [InlineData("Damper", 24)]
+    [InlineData("Pyramid-5", 26)]
+    [InlineData("Last Type", 26)]
+    [InlineData("1D", 27)]
+    [InlineData("2D", 28)]
+    [InlineData("3D", 29)]
+    [InlineData("1D Section", 30)]
+    [InlineData("2D Finite Element", 31)]
+    [InlineData("2d FinitEelement", 31)]
+    [InlineData("2D Load", 32)]
+    [InlineData("2dLoad", 32)]
+    public void GetElementTypeTest(string input, int expected)
     {
       // Act
       ElementType actual = Mappings.GetElementType(input);
 
       // Assert
-      Assert.Equal(expected, actual);
+      Assert.Equal(expected, (int)actual);
     }
 
     [Theory]
-    [InlineData("Undefined", MatType.UNDEF)]
-    [InlineData("None", MatType.NONE)]
-    [InlineData("Generic", MatType.GENERIC)]
-    [InlineData("Steel", MatType.STEEL)]
-    [InlineData("Concrete", MatType.CONCRETE)]
-    [InlineData("Aluminium", MatType.ALUMINIUM)]
-    [InlineData("Glass", MatType.GLASS)]
-    [InlineData("FRP", MatType.FRP)]
-    [InlineData("Rebar", MatType.REBAR)]
-    [InlineData("Timber", MatType.TIMBER)]
-    [InlineData("Fabric", MatType.FABRIC)]
-    [InlineData("Soil", MatType.SOIL)]
-    [InlineData("Numeric Material", MatType.NUM_MT)]
-    [InlineData("Compound", MatType.COMPOUND)]
-    [InlineData("Bar", MatType.BAR)]
-    [InlineData("Tendon", MatType.TENDON)]
-    [InlineData("FRP Bar", MatType.FRPBAR)]
-    [InlineData("CFRP", MatType.CFRP)]
-    [InlineData("GFRP", MatType.GFRP)]
-    [InlineData("AFRP", MatType.AFRP)]
-    [InlineData("ARGFRP", MatType.ARGFRP)]
-    [InlineData("Bar Material", MatType.BARMAT)]
-    public void GetMatTypeTest(string input, MatType expected)
+    [InlineData("Undefined", -2)]
+    [InlineData("None", -1)]
+    [InlineData("Generic", 0)]
+    [InlineData("Steel", 1)]
+    [InlineData("Concrete", 2)]
+    [InlineData("Aluminium", 3)]
+    [InlineData("Glass", 4)]
+    [InlineData("FRP", 5)]
+    [InlineData("Rebar", 6)]
+    [InlineData("Timber", 7)]
+    [InlineData("Fabric", 8)]
+    [InlineData("Soil", 9)]
+    [InlineData("Numeric Material", 10)]
+    [InlineData("Compound", 0x100)]
+    [InlineData("Bar", 0x1000)]
+    [InlineData("Tendon", 4352)]
+    [InlineData("FRP Bar", 4608)]
+    [InlineData("CFRP", 4864)]
+    [InlineData("GFRP", 5120)]
+    [InlineData("AFRP", 5376)]
+    [InlineData("ARGFRP", 5632)]
+    [InlineData("Bar Material", 65280)]
+    public void GetMatTypeTest(string input, int expected)
     {
       // Act
       MatType actual = Mappings.GetMatType(input);
 
       // Assert
-      Assert.Equal(expected, actual);
+      Assert.Equal(expected, (int) actual);
     }
 
     [Theory]
-    [InlineData("Undefined", MemberType.UNDEF)]
-    [InlineData("Generic 1D", MemberType.GENERIC_1D)]
-    [InlineData("Generic 2D", MemberType.GENERIC_2D)]
-    [InlineData("Beam", MemberType.BEAM)]
-    [InlineData("Column", MemberType.COLUMN)]
-    [InlineData("Slab", MemberType.SLAB)]
-    [InlineData("Wall", MemberType.WALL)]
-    [InlineData("Cantilever", MemberType.CANTILEVER)]
-    [InlineData("Ribbed Slab", MemberType.RIBSLAB)]
-    [InlineData("Composite", MemberType.COMPOS)]
-    [InlineData("Pile", MemberType.PILE)]
-    [InlineData("Explicit", MemberType.EXPLICIT)]
-    [InlineData("1D Void Cutter", MemberType.VOID_CUTTER_1D)]
-    [InlineData("2D Void Cutter", MemberType.VOID_CUTTER_2D)]
-    [InlineData("Generic 3D", MemberType.GENERIC_3D)]
-    public void GetMemberTypeest(string input, MemberType expected)
+    [InlineData("Undefined", -1)]
+    [InlineData("Generic 1D", 0)]
+    [InlineData("Generic 2D", 1)]
+    [InlineData("Beam", 2)]
+    [InlineData("Column", 3)]
+    [InlineData("Slab", 4)]
+    [InlineData("Wall", 5)]
+    [InlineData("Cantilever", 6)]
+    [InlineData("Ribbed Slab", 7)]
+    [InlineData("Composite", 8)]
+    [InlineData("Pile", 9)]
+    [InlineData("Explicit", 10)]
+    [InlineData("1D Void Cutter", 11)]
+    [InlineData("2D Void Cutter", 12)]
+    [InlineData("Generic 3D", 13)]
+    public void GetMemberType(string input, int expected)
     {
       // Act
       MemberType actual = Mappings.GetMemberType(input);
 
       // Assert
-      Assert.Equal(expected, actual);
+      Assert.Equal(expected, (int)actual);
     }
 
     [Theory]
-    [InlineData("Undefined", Property2D_Type.UNDEF)]
-    [InlineData("Plane Stress", Property2D_Type.PL_STRESS)]
-    [InlineData("Plane Strain", Property2D_Type.PL_STRAIN)]
-    [InlineData("Axis Symmetric", Property2D_Type.AXISYMMETRIC)]
-    [InlineData("Fabric", Property2D_Type.FABRIC)]
-    [InlineData("Plate", Property2D_Type.PLATE)]
-    [InlineData("Shell", Property2D_Type.SHELL)]
-    [InlineData("Curved Shell", Property2D_Type.CURVED_SHELL)]
-    [InlineData("Torsion", Property2D_Type.TORSION)]
-    [InlineData("Load Panel", Property2D_Type.LOAD)]
-    [InlineData("Num Type", Property2D_Type.NUM_TYPE)]
-    public void GetProperty2D_TypeTest(string input, Property2D_Type expected)
+    [InlineData("Undefined", 0)]
+    [InlineData("Plane Stress", 1)]
+    [InlineData("Plane Strain", 2)]
+    [InlineData("Axis Symmetric", 3)]
+    [InlineData("Fabric", 4)]
+    [InlineData("Plate", 5)]
+    [InlineData("Shell", 6)]
+    [InlineData("Curved Shell", 7)]
+    [InlineData("Torsion", 8)]
+    [InlineData("Load Panel", 10)]
+    [InlineData("Num Type", 11)]
+    public void GetProperty2D_TypeTest(string input, int expected)
     {
       // Act
       Property2D_Type actual = Mappings.GetProperty2D_Type(input);
 
       // Assert
-      Assert.Equal(expected, actual);
+      Assert.Equal(expected, (int) actual);
     }
   }
 }
