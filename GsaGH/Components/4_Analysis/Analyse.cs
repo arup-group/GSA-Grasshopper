59f2b584c24d70afa01649af31b3f4b124cd15fd
kpne <Kristjan.Nielsen@arup.com>
2023-02-02 18:47:27 +0100

    fix tolerance always updated in meters

--- GsaGH/Components/4_Analysis/Analyse.cs
+++ GsaGH/Components/4_Analysis/Analyse.cs
@@ -192,7 +192,7 @@ namespace GsaGH.Components
     private List<bool> InitialCheckState = new List<bool>() { true, true };
     private bool Analysis = true;
     private bool ReMesh = true;
-    private double _tolerance = DefaultUnits.Tolerance.Meters;
+    private double _tolerance = DefaultUnits.Tolerance.As(DefaultUnits.LengthUnitGeometry);
     private string _toleranceTxt = "";
 
     protected override void BeforeSolveInstance()
@@ -255,8 +255,8 @@ namespace GsaGH.Components
       Menu_AppendSeparator(menu);
 
       ToolStripTextBox tolerance = new ToolStripTextBox();
-      _toleranceTxt = new Length(_tolerance, this.LengthUnit).ToString();
-      tolerance.Text = _toleranceTxt;
+      this._toleranceTxt = new Length(_tolerance, this.LengthUnit).ToString();
+      tolerance.Text = this._toleranceTxt;
       tolerance.BackColor = System.Drawing.Color.FromArgb(255, 180, 255, 150);
       tolerance.TextChanged += (s, e) => MaintainText(tolerance);
 
@@ -280,7 +280,7 @@ namespace GsaGH.Components
     }
     private void MaintainText(ToolStripTextBox tolerance)
     {
-      _toleranceTxt = tolerance.Text;
+      this._toleranceTxt = tolerance.Text;
       if (Length.TryParse(_toleranceTxt, out Length res))
         tolerance.BackColor = System.Drawing.Color.FromArgb(255, 180, 255, 150);
       else
@@ -293,7 +293,7 @@ namespace GsaGH.Components
         try
         {
           Length newTolerance = Length.Parse(_toleranceTxt);
-          _tolerance = newTolerance.Meters;
+          this._tolerance = newTolerance.As(this.LengthUnit);
         }
         catch (Exception e)
         {
@@ -301,7 +301,7 @@ namespace GsaGH.Components
           return;
         }
       }
-      Length tol = new Length(_tolerance, this.LengthUnit);
+      Length tol = new Length(this._tolerance, this.LengthUnit);
       this.Message = "Tol: " + tol.ToString();
       if (tol.Meters < 0.001)
         AddRuntimeMessage(GH_RuntimeMessageLevel.Remark, "Set tolerance is quite small, you can change this by right-clicking the component.");
