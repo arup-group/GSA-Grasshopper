add3f9adc66be283177f05e1b5c619bb5ac718c5
Tilman Reinhardt <tilman.reinhardt@arup.com>
2023-02-27 16:57:33 +0000

    file name

--- IntegrationTests/3_Components/ReactionForceDiagramsTests.cs
+++ IntegrationTests/3_Components/ReactionForceDiagramsTests.cs
@@ -8,13 +8,13 @@ using Xunit;
 namespace IntegrationTests.Components
 {
   [Collection("GrasshopperFixture collection")]
-  public class ReactionForceDiagramsTest
+  public class ReactionForceDiagramsTests
   {
     public static GH_Document Document()
     {
       Type thisClass = MethodBase.GetCurrentMethod().DeclaringType;
       string fileName = thisClass.Name + ".gh";
-      fileName = fileName.Replace(thisClass.Namespace, string.Empty).Replace("Test", string.Empty);
+      fileName = fileName.Replace(thisClass.Namespace, string.Empty).Replace("Tests", string.Empty);
 
       string solutiondir = Directory.GetParent(Directory.GetCurrentDirectory()).Parent.Parent.Parent.Parent.FullName;
       string path = Path.Combine(new string[] { solutiondir, "ExampleFiles", "Components" });
