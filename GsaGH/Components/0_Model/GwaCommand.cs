ca27aa68357b05507dbb2c1255e2ed4819765435
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-03-07 20:28:38 +0100

    move down the exposure level

--- GsaGH/Components/0_Model/GwaCommand.cs
+++ GsaGH/Components/0_Model/GwaCommand.cs
@@ -16,7 +16,7 @@ namespace GsaGH.Components
   {
     #region Name and Ribbon Layout
     public override Guid ComponentGuid => new Guid("ed3e5d61-9942-49d4-afc7-310285c783c6");
-    public override GH_Exposure Exposure => GH_Exposure.tertiary | GH_Exposure.obscure;
+    public override GH_Exposure Exposure => GH_Exposure.quarternary | GH_Exposure.obscure;
     protected override System.Drawing.Bitmap Icon => GsaGH.Properties.Resources.GwaModel;
     public override OasysPluginInfo PluginInfo => GsaGH.PluginInfo.Instance;
     protected override string HtmlHelp_Source()
