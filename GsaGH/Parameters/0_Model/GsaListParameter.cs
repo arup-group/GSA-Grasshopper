aa113f224bf6f84abf21ec28a71bfd4e7a905dd1
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-03-07 20:28:52 +0100

    change exposure to obscure

--- GsaGH/Parameters/0_Model/GsaListParameter.cs
+++ GsaGH/Parameters/0_Model/GsaListParameter.cs
@@ -13,7 +13,7 @@ namespace GsaGH.Parameters
     public override string InstanceDescription => this.m_data.DataCount == 0 ? "Empty " + GsaListGoo.Name + " parameter" : base.InstanceDescription;
     public override string TypeName => this.SourceCount == 0 ? GsaListGoo.Name : base.TypeName;
     public override Guid ComponentGuid => new Guid("ce4c0131-22bd-4046-8b69-c42832cf7a53");
-    public override GH_Exposure Exposure => GH_Exposure.primary;
+    public override GH_Exposure Exposure => GH_Exposure.primary | GH_Exposure.obscure;
     protected override System.Drawing.Bitmap Icon => GsaGH.Properties.Resources.ListParam;
 
     public GsaListParameter() : base(new GH_InstanceDescription(
