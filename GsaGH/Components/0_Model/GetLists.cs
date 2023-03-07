ba3cfd76cd717b1287f47883e1da05c2e777e67e
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-03-07 20:30:31 +0100

    change exposure and edit name, nickname and description

--- GsaGH/Components/0_Model/GetLists.cs
+++ GsaGH/Components/0_Model/GetLists.cs
@@ -18,13 +18,13 @@ namespace GsaGH.Components
   {
     #region Name and Ribbon Layout
     public override Guid ComponentGuid => new Guid("43495cf4-f2eb-4b14-9b1a-5f91972546ca");
-    public override GH_Exposure Exposure => GH_Exposure.secondary;
+    public override GH_Exposure Exposure => GH_Exposure.tertiary | GH_Exposure.obscure;
     public override OasysPluginInfo PluginInfo => GsaGH.PluginInfo.Instance;
     protected override System.Drawing.Bitmap Icon => GsaGH.Properties.Resources.GetList;
 
-    public GetLists() : base("Get Model Properties",
-      "GetProps",
-      "Get Sections, 2D Properties and Springs from GSA model",
+    public GetLists() : base("Get Model Lists",
+      "GetLists",
+      "Get Entity Lists from GSA model",
       CategoryName.Name(),
       SubCategoryName.Cat0())
     { this.Hidden = true; } // sets the initial state of the component to hidden
