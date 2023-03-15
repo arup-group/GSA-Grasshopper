9a19ed88d58639984b66fc45f46538893d237ee7
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-03-15 18:26:46 +0100

    use switch case and added warning

--- GsaGH/Components/2_Geometry/ShowId.cs
+++ GsaGH/Components/2_Geometry/ShowId.cs
@@ -14,12 +14,10 @@ using OasysUnits;
 using GsaAPI;
 using Rhino.Collections;
 using Grasshopper.Kernel.Data;
+using OasysGH.Helpers;
 
 namespace GsaGH.Components
 {
-  /// <summary>
-  /// Component to edit a Node
-  /// </summary>
   public class ShowId : GH_OasysComponent, IGH_PreviewObject
   {
     private List<Point3d> _pts;
@@ -58,111 +56,104 @@ namespace GsaGH.Components
       // clear cached preview data
       _pts = new List<Point3d>();
       _txts = new List<string>();
-      
+
       if (DA.GetDataTree(0, out GH_Structure<IGH_Goo> tree))
       {
         // component outputs
         var ids = new GH_Structure<GH_Integer>();
         var ghPts = new GH_Structure<GH_Point>();
 
-        foreach (var path in tree.Paths) 
+        foreach (var path in tree.Paths)
         {
           foreach (IGH_Goo goo in tree.get_Branch(path))
           {
-            if (goo is GsaElement2dGoo e2d)
-            {
-              for (int i = 0; i < e2d.Value.Mesh.Faces.Count; i++)
-              {
-                _txts.Add(e2d.Value.Ids[i].ToString());
-                ids.Append(new GH_Integer(e2d.Value.Ids[i]), path);
-                _pts.Add(e2d.Value.Mesh.Faces.GetFaceCenter(i));
-                ghPts.Append(new GH_Point(e2d.Value.Mesh.Faces.GetFaceCenter(i)), path);
-              }
-              continue;
-            }
-
-            if (goo is GsaElement3dGoo e3d)
-            {
-              for (int i = 0; i < e3d.Value.NgonMesh.Ngons.Count; i++)
-              {
-                _txts.Add(e3d.Value.Ids[i].ToString());
-                ids.Append(new GH_Integer(e3d.Value.Ids[i]), path);
-                _pts.Add(e3d.Value.NgonMesh.Ngons.GetNgonCenter(i));
-                ghPts.Append(new GH_Point(e3d.Value.NgonMesh.Ngons.GetNgonCenter(i)), path);
-              }
-              continue;
-            }
-
-            if (goo is MeshResultGoo resMesh)
-            {
-              for (int i = 0; i < resMesh.ElementIds.Count; i++)
-              {
-                _txts.Add(resMesh.ElementIds[i].ToString());
-                ids.Append(new GH_Integer(resMesh.ElementIds[i]), path);
-                if (resMesh.Value.Ngons.Count > 0)
-                { 
-                  _pts.Add(resMesh.Value.Ngons.GetNgonCenter(i));
-                  ghPts.Append(new GH_Point(resMesh.Value.Ngons.GetNgonCenter(i)), path);
-                }
-                else
-                {
-                  _pts.Add(resMesh.Value.Faces.GetFaceCenter(i));
-                  ghPts.Append(new GH_Point(resMesh.Value.Faces.GetFaceCenter(i)), path);
-                }
-              }
-              continue;
-            }
-
             int id = 0;
             Point3d pt = Point3d.Unset;
 
-            if (goo is GsaNodeGoo node)
+            switch (goo)
             {
-              id = node.Value.Id;
-              pt = node.Value.Point;
-            }
+              case GsaElement2dGoo e2d:
+                for (int i = 0; i < e2d.Value.Mesh.Faces.Count; i++)
+                {
+                  _txts.Add(e2d.Value.Ids[i].ToString());
+                  ids.Append(new GH_Integer(e2d.Value.Ids[i]), path);
+                  _pts.Add(e2d.Value.Mesh.Faces.GetFaceCenter(i));
+                  ghPts.Append(new GH_Point(e2d.Value.Mesh.Faces.GetFaceCenter(i)), path);
+                }
+                continue;
 
-            if (goo is GsaElement1dGoo e1d)
-            {
-              id = e1d.Value.Id;
-              pt = e1d.Value.Line.PointAtNormalizedLength(0.5);
-            }
+              case GsaElement3dGoo e3d:
+                for (int i = 0; i < e3d.Value.NgonMesh.Ngons.Count; i++)
+                {
+                  _txts.Add(e3d.Value.Ids[i].ToString());
+                  ids.Append(new GH_Integer(e3d.Value.Ids[i]), path);
+                  _pts.Add(e3d.Value.NgonMesh.Ngons.GetNgonCenter(i));
+                  ghPts.Append(new GH_Point(e3d.Value.NgonMesh.Ngons.GetNgonCenter(i)), path);
+                }
+                continue;
 
-            if (goo is GsaMember1dGoo m1d)
-            {
-              id = m1d.Value.Id;
-              pt = m1d.Value.PolyCurve.PointAtNormalizedLength(0.5);
-            }
+              case MeshResultGoo resMesh:
+                for (int i = 0; i < resMesh.ElementIds.Count; i++)
+                {
+                  _txts.Add(resMesh.ElementIds[i].ToString());
+                  ids.Append(new GH_Integer(resMesh.ElementIds[i]), path);
+                  if (resMesh.Value.Ngons.Count > 0)
+                  {
+                    _pts.Add(resMesh.Value.Ngons.GetNgonCenter(i));
+                    ghPts.Append(new GH_Point(resMesh.Value.Ngons.GetNgonCenter(i)), path);
+                  }
+                  else
+                  {
+                    _pts.Add(resMesh.Value.Faces.GetFaceCenter(i));
+                    ghPts.Append(new GH_Point(resMesh.Value.Faces.GetFaceCenter(i)), path);
+                  }
+                }
+                continue;
 
-            if (goo is GsaMember2dGoo m2d)
-            {
-              id = m2d.Value.Id;
-              m2d.Value.PolyCurve.TryGetPolyline(out Polyline pl);
-              pt = pl.CenterPoint();
-            }
+              case GsaNodeGoo node:
+                id = node.Value.Id;
+                pt = node.Value.Point;
+                break;
 
-            if (goo is GsaMember3dGoo m3d)
-            {
-              id = m3d.Value.Id;
-              pt = m3d.Value.SolidMesh.GetBoundingBox(false).Center;
-            }
+              case GsaElement1dGoo e1d:
+                id = e1d.Value.Id;
+                pt = e1d.Value.Line.PointAtNormalizedLength(0.5);
+                break;
 
-            if (goo is PointResultGoo resPoint)
-            {
-              id = resPoint.NodeId;
-              pt = resPoint.Value;
-            }
+              case GsaMember1dGoo m1d:
+                id = m1d.Value.Id;
+                pt = m1d.Value.PolyCurve.PointAtNormalizedLength(0.5);
+                break;
 
-            if (goo is LineResultGoo resLine)
-            {
-              id = resLine.ElementId;
-              pt = resLine.Value.PointAt(0.5);
-            }
+              case GsaMember2dGoo m2d:
+                id = m2d.Value.Id;
+                m2d.Value.PolyCurve.TryGetPolyline(out Polyline pl);
+                pt = pl.CenterPoint();
+                break;
 
-            if (goo is VectorResultGoo resVector)
-            {
-              id = resVector.NodeId;
-              pt = resVector.StartingPoint;
+              case GsaMember3dGoo m3d:
+                id = m3d.Value.Id;
+                pt = m3d.Value.SolidMesh.GetBoundingBox(false).Center;
+                break;
+
+              case PointResultGoo resPoint:
+                id = resPoint.NodeId;
+                pt = resPoint.Value;
+                break;
+
+              case LineResultGoo resLine:
+                id = resLine.ElementId;
+                pt = resLine.Value.PointAt(0.5);
+                break;
+
+              case VectorResultGoo resVector:
+                id = resVector.NodeId;
+                pt = resVector.StartingPoint;
+                break;
+
+              default:
+                this.AddRuntimeWarning("Unable to convert " + goo.TypeName + " to Node, Element (1D/2D/3D), Member (1D/2D/3D) or Point/Line/Mesh result.");
+                break;
             }
 
             _txts.Add(id.ToString());
@@ -183,7 +174,7 @@ namespace GsaGH.Components
 
       if (_txts != null)
       {
-        for(int i = 0; i < _txts.Count; i++)
+        for (int i = 0; i < _txts.Count; i++)
         {
           var positionOnTheScreen = args.Viewport.WorldToClient(_pts[i]);
           args.Display.Draw2dText(_txts[i], this.Attributes.Selected ? args.WireColour_Selected : args.WireColour, positionOnTheScreen, true);
