e54db473936eec2ddda0be4851488a4b956f51a9
Dominika Los <dominika.los@arup.com>
2023-03-27 18:29:34 +0200

    gsagh-248 gsaghTests/GooWrappers dir clean-up

--- GsaGHTests/2_GooWrappers/GH_OasysGooTest.cs
+++ GsaGHTests/2_GooWrappers/GH_OasysGooTest.cs
@@ -1,15 +1,13 @@
 ﻿using System;
 using System.Reflection;
-using GsaGHTests.Helpers;
 using Grasshopper.Kernel.Types;
 using GsaGH.Parameters;
+using GsaGHTests.Helpers;
 using Xunit;
 
-namespace GsaGHTests.GooWrappers
-{
+namespace GsaGHTests.GooWrappers {
   [Collection("GrasshopperFixture collection")]
-  public class GH_OasysGooTest
-  {
+  public class GhOasysGooTest {
     [Theory]
     [InlineData(typeof(GsaModelGoo), typeof(GsaModel))]
     [InlineData(typeof(GsaBool6Goo), typeof(GsaBool6))]
@@ -23,21 +21,17 @@ namespace GsaGHTests.GooWrappers
     [InlineData(typeof(GsaAnalysisCaseGoo), typeof(GsaAnalysisCase))]
     [InlineData(typeof(GsaAnalysisTaskGoo), typeof(GsaAnalysisTask))]
     [InlineData(typeof(GsaCombinationCaseGoo), typeof(GsaCombinationCase))]
-    public void GenericGH_OasysGooTest(Type gooType, Type wrapType)
-    {
-      // Create the actual API object
-      Object value = Activator.CreateInstance(wrapType);
-      Object[] parameters = { value };
+    public void GenericGH_OasysGooTest(Type gooType, Type wrapType) {
+      object value = Activator.CreateInstance(wrapType);
+      object[] parameters = {
+        value,
+      };
 
-      // Create GH_OasysGoo<API_Object> 
-      Object objectGoo = Activator.CreateInstance(gooType, parameters);
+      object objectGoo = Activator.CreateInstance(gooType, parameters);
       gooType = objectGoo.GetType();
 
-      // Trigger the IGH_Goo Duplicate() method
       IGH_Goo duplicate = ((IGH_Goo)objectGoo).Duplicate();
-      // check that they are equal
       Duplicates.AreEqual(duplicate, objectGoo);
-      // check that they are not the same object (same pointer in memory)
       Assert.NotEqual(duplicate, objectGoo);
 
       bool hasValue = false;
@@ -46,55 +40,43 @@ namespace GsaGHTests.GooWrappers
       bool hasNickName = false;
       bool hasDescription = false;
 
-      // we can't cast directly to objectGoo.Value, so we do this instead
-      PropertyInfo[] gooPropertyInfo = gooType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
-      foreach (PropertyInfo gooProperty in gooPropertyInfo)
-      {
-        if (gooProperty.Name == "Value")
-        {
-          Object gooValue = gooProperty.GetValue(objectGoo, null);
-          // here check that the value in the goo object is a duplicate of the original object
+      PropertyInfo[] gooPropertyInfo
+        = gooType.GetProperties(BindingFlags.Public | BindingFlags.Instance | BindingFlags.Static);
+      foreach (PropertyInfo gooProperty in gooPropertyInfo) {
+        if (gooProperty.Name == "Value") {
+          object gooValue = gooProperty.GetValue(objectGoo, null);
           Duplicates.AreEqual(value, gooValue);
-          // check that they are not the same object (same pointer in memory)
           Assert.NotEqual(value, gooValue);
 
           hasValue = true;
         }
 
-        // check the grasshopper tostring method (when you hover over the input/output)
-        if (gooProperty.Name == "TypeName")
-        {
+        if (gooProperty.Name == "TypeName") {
           string typeName = (string)gooProperty.GetValue(objectGoo, null);
           Assert.StartsWith("GSA " + typeName + " (", objectGoo.ToString());
           hasToString = true;
         }
 
-        // check the name, input/output parameters
-        if (gooProperty.Name == "Name")
-        {
+        if (gooProperty.Name == "Name") {
           string name = (string)gooProperty.GetValue(objectGoo, null);
-          // require a name longer than 3 characters (stud being the shortest accepted)
           Assert.True(name.Length > 3);
           hasName = true;
         }
-        // check the nickname, input/output parameters
-        if (gooProperty.Name == "NickName")
-        {
+
+        if (gooProperty.Name == "NickName") {
           string nickName = (string)gooProperty.GetValue(objectGoo, null);
-          // require a nickname not longer than 3 characters excluding dots
           nickName = nickName.Replace(".", string.Empty);
           Assert.True(nickName.Length < 4);
           Assert.True(nickName.Length > 0);
           hasNickName = true;
         }
-        if (gooProperty.Name == "Description")
-        {
-          string description = (string)gooProperty.GetValue(objectGoo, null);
-          // require a description to start with "GSA"
-          Assert.StartsWith("GSA ", description);
-          Assert.True(description.Length > 7);
-          hasDescription = true;
-        }
+
+        if (gooProperty.Name != "Description")
+          continue;
+        string description = (string)gooProperty.GetValue(objectGoo, null);
+        Assert.StartsWith("GSA ", description);
+        Assert.True(description.Length > 7);
+        hasDescription = true;
       }
 
       Assert.True(hasValue);
--- GsaGHTests/2_GooWrappers/GsaBool6GooTest.cs
+++ GsaGHTests/2_GooWrappers/GsaBool6GooTest.cs
@@ -8,13 +8,10 @@ namespace GsaGHTests.GooWrappers {
     [InlineData(true)]
     [InlineData(false)]
     public void CastFromBoolTest(bool b) {
-      // Arrange
       var goo = new GsaBool6Goo(new GsaBool6());
 
-      // Act
       goo.CastFrom(b);
 
-      // Assert
       Assert.Equal(b, goo.Value.X);
       Assert.Equal(b, goo.Value.Y);
       Assert.Equal(b, goo.Value.Z);
@@ -24,32 +21,108 @@ namespace GsaGHTests.GooWrappers {
     }
 
     [Theory]
-    [InlineData("free", false, false, false, false, false, false)]
-    [InlineData("pin", true, true, true, false, false, false)]
-    [InlineData("pinned", true, true, true, false, false, false)]
-    [InlineData("fix", true, true, true, true, true, true)]
-    [InlineData("fixed", true, true, true, true, true, true)]
-    [InlineData("release", false, false, false, false, true, true)]
-    [InlineData("released", false, false, false, false, true, true)]
-    [InlineData("hinge", false, false, false, false, true, true)]
-    [InlineData("hinged", false, false, false, false, true, true)]
-    [InlineData("charnier", false, false, false, false, true, true)]
-    [InlineData("rrrrrr", false, false, false, false, false, false)]
-    [InlineData("ffffff", true, true, true, true, true, true)]
-    public void CastFromStringtest(string s, bool expectedX, bool expectedY, bool expectedZ, bool expectedXX, bool expectedYY, bool expectedZZ) {
-      // Arrange
+    [InlineData("free",
+      false,
+      false,
+      false,
+      false,
+      false,
+      false)]
+    [InlineData("pin",
+      true,
+      true,
+      true,
+      false,
+      false,
+      false)]
+    [InlineData("pinned",
+      true,
+      true,
+      true,
+      false,
+      false,
+      false)]
+    [InlineData("fix",
+      true,
+      true,
+      true,
+      true,
+      true,
+      true)]
+    [InlineData("fixed",
+      true,
+      true,
+      true,
+      true,
+      true,
+      true)]
+    [InlineData("release",
+      false,
+      false,
+      false,
+      false,
+      true,
+      true)]
+    [InlineData("released",
+      false,
+      false,
+      false,
+      false,
+      true,
+      true)]
+    [InlineData("hinge",
+      false,
+      false,
+      false,
+      false,
+      true,
+      true)]
+    [InlineData("hinged",
+      false,
+      false,
+      false,
+      false,
+      true,
+      true)]
+    [InlineData("charnier",
+      false,
+      false,
+      false,
+      false,
+      true,
+      true)]
+    [InlineData("rrrrrr",
+      false,
+      false,
+      false,
+      false,
+      false,
+      false)]
+    [InlineData("ffffff",
+      true,
+      true,
+      true,
+      true,
+      true,
+      true)]
+    public void CastFromStringtest(
+      string s,
+      bool expectedX,
+      bool expectedY,
+      bool expectedZ,
+      bool expectedXx,
+      bool expectedYy,
+      bool expectedZz) {
       var goo = new GsaBool6Goo(new GsaBool6());
 
-      // Act
       goo.CastFrom(s);
 
-      // Assert
       Assert.Equal(expectedX, goo.Value.X);
       Assert.Equal(expectedY, goo.Value.Y);
       Assert.Equal(expectedZ, goo.Value.Z);
-      Assert.Equal(expectedXX, goo.Value.Xx);
-      Assert.Equal(expectedYY, goo.Value.Yy);
-      Assert.Equal(expectedZZ, goo.Value.Zz);
+      Assert.Equal(expectedXx, goo.Value.Xx);
+      Assert.Equal(expectedYy, goo.Value.Yy);
+      Assert.Equal(expectedZz, goo.Value.Zz);
     }
   }
 }
