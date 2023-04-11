e4e80bbbd6af08198dca3d777a989f6134f71468
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-04-11 18:57:35 +0200

    format

--- GsaGH/Helpers/Import/Lists.cs
+++ GsaGH/Helpers/Import/Lists.cs
@@ -16,7 +16,7 @@ namespace GsaGH.Helpers.Import
   {
     internal static List<GsaList> GetLists(GsaModel gsaModel)
     {
-      List<GsaList> lists = new List<GsaList>();
+      var lists = new List<GsaList>();
       foreach (KeyValuePair<int, EntityList> apiList in gsaModel.Model.Lists())
         lists.Add(new GsaList(apiList.Key, apiList.Value, gsaModel));
       return lists;
