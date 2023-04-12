4718fd8c54c808f95741a1089eda55aef33a4ddf
Tilman Reinhardt <tilman.reinhardt@arup.com>
2023-04-12 15:46:40 +0200

    fixture

--- GsaGHTests/GrasshopperFixture.cs
+++ GsaGHTests/GrasshopperFixture.cs
@@ -5,15 +5,29 @@ using Xunit;
 
 namespace GsaGHTests {
   public class GrasshopperFixture : IDisposable {
+    public Rhino.Runtime.InProcess.RhinoCore Core {
+      get {
+        if (null == _core)
+          InitializeCore();
+        return _core as Rhino.Runtime.InProcess.RhinoCore;
+      }
+    }
+    public Grasshopper.Plugin.GH_RhinoScriptInterface GhPlugin {
+      get {
+        if (null == _ghPlugin)
+          InitializeGrasshopperPlugin();
+        return _ghPlugin as Grasshopper.Plugin.GH_RhinoScriptInterface;
+      }
+    }
     public static string InstallPath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles), "Oasys", "GSA 10.1");
 
+    private object Doc { get; set; }
+    private object DocIo { get; set; }
+    private const string LinkFileName = "GsaGhTests.ghlink";
+    private static readonly string s_linkFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Grasshopper", "Libraries");
     private object _core = null;
     private object _ghPlugin = null;
-    private object DocIo { get; set; }
-    private object Doc { get; set; }
     private bool _isDisposed;
-    private static readonly string s_linkFilePath = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "Grasshopper", "Libraries");
-    private const string LinkFileName = "GsaGhTests.ghlink";
 
     static GrasshopperFixture() {
       // This MUST be included in a static constructor to ensure that no Rhino DLLs
@@ -22,7 +36,6 @@ namespace GsaGHTests {
       // assemblies to be loaded before this is called.
       RhinoInside.Resolver.Initialize();
     }
-
 
     public GrasshopperFixture() {
       AddPluginToGh();
@@ -35,19 +48,25 @@ namespace GsaGHTests {
       OasysGH.Units.Utility.SetupUnitsDuringLoad(true);
     }
 
+    public void AddPluginToGh() {
+      Directory.CreateDirectory(s_linkFilePath);
+      StreamWriter writer = File.CreateText(Path.Combine(s_linkFilePath, LinkFileName));
+      writer.Write(Environment.CurrentDirectory);
+      writer.Close();
+    }
+
+    public void Dispose() {
+      Dispose(disposing: true);
+      GC.SuppressFinalize(this);
+      File.Delete(Path.Combine(s_linkFilePath, LinkFileName));
+    }
+
     public void LoadRefs() {
       const string name = "PATH";
       string pathvar = System.Environment.GetEnvironmentVariable(name);
       string value = pathvar + ";" + InstallPath + "\\";
       EnvironmentVariableTarget target = EnvironmentVariableTarget.Process;
       System.Environment.SetEnvironmentVariable(name, value, target);
-    }
-
-    public void AddPluginToGh() {
-      Directory.CreateDirectory(s_linkFilePath);
-      StreamWriter writer = File.CreateText(Path.Combine(s_linkFilePath, LinkFileName));
-      writer.Write(Environment.CurrentDirectory);
-      writer.Close();
     }
 
     protected virtual void Dispose(bool disposing) {
@@ -62,28 +81,6 @@ namespace GsaGHTests {
       }
 
       _isDisposed = true;
-    }
-
-    public void Dispose() {
-      Dispose(disposing: true);
-      GC.SuppressFinalize(this);
-      File.Delete(Path.Combine(s_linkFilePath, LinkFileName));
-    }
-
-    public Rhino.Runtime.InProcess.RhinoCore Core {
-      get {
-        if (null == _core)
-          InitializeCore();
-        return _core as Rhino.Runtime.InProcess.RhinoCore;
-      }
-    }
-
-    public Grasshopper.Plugin.GH_RhinoScriptInterface GhPlugin {
-      get {
-        if (null == _ghPlugin)
-          InitializeGrasshopperPlugin();
-        return _ghPlugin as Grasshopper.Plugin.GH_RhinoScriptInterface;
-      }
     }
 
     private void InitializeCore() => _core = new Rhino.Runtime.InProcess.RhinoCore();
