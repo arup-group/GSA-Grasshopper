8ab4894c36563167f993edc9124e2cfa39003229
Dominika Los <dominika.los@arup.com>
2023-04-24 10:44:49 +0200

    gsagh-267 added mapping for diagramType

---
+++ GsaGH/Helpers/GsaAPI/EnumMappings/DiagramTypeMapping.cs
@@ -0,0 +1,11 @@
+﻿using GsaAPI;
+using Diagram = GsaGH.Parameters.Enums;
+
+namespace GsaGH.Helpers.GsaAPI.EnumMappings {
+  public class DiagramTypeMapping : EnumMapping<DiagramType, Diagram.DiagramType> {
+
+    public DiagramTypeMapping(
+      string description, DiagramType gsaApiEnum, Diagram.DiagramType gsaGhEnum) : base(description,
+      gsaApiEnum, gsaGhEnum) { }
+  }
+}
