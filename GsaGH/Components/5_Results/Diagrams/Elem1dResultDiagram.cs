ab2735f53c5647334bd5ac40d88c9e146b690577
Dominika Los <dominika.los@arup.com>
2023-04-27 17:36:10 +0200

    gsagh-267 elem1d diagram scaling

--- GsaGH/Components/5_Results/Diagrams/Elem1dResultDiagram.cs
+++ GsaGH/Components/5_Results/Diagrams/Elem1dResultDiagram.cs
@@ -1,8 +1,10 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Drawing;
 using System.Linq;
+using System.Windows.Forms;
 using GH_IO.Serialization;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
@@ -14,6 +16,11 @@ using GsaGH.Properties;
 using OasysGH;
 using OasysGH.Components;
 using OasysGH.UI;
+using OasysGH.Units;
+using OasysGH.Units.Helpers;
+using OasysUnits;
+using OasysUnits.Units;
+using Rhino.Geometry;
 using DiagramType = GsaGH.Parameters.Enums.DiagramType;
 using Line = Rhino.Geometry.Line;
 
@@ -27,7 +34,15 @@ namespace GsaGH.Components {
       public Color Color { get; private set; }
       //public IQuantity Quantity { get; private set; } // will be used when we can get the values from graphicDrawResult
 
-      public DiagramLine(Line line, Color color /*, IQuantity quantity*/) {
+      public DiagramLine(Point3d startPoint, Point3d endPoint, Color color) {
+        Line = new Line(startPoint, endPoint);
+        Color = color;
+        //Quantity = quantity;
+      }
+
+      public DiagramLine(Line line, double multipier, Color color) {
+        line.From *= multipier;
+        line.To *= multipier;
         Line = line;
         Color = color;
         //Quantity = quantity;
@@ -42,8 +57,14 @@ namespace GsaGH.Components {
     private string _case = "";
     private double _defScale = 250;
     private DiagramType _displayedDiagramType = DiagramType.AxialForceFx;
+    private ForceUnit _forceUnit = DefaultUnits.ForceUnit;
+    private LengthUnit _lengthResultUnit = DefaultUnits.LengthUnitResult;
+    private LengthUnit _lengthUnit = DefaultUnits.LengthUnitGeometry;
+    private MomentUnit _momentUnit = DefaultUnits.MomentUnit;
+    private PressureUnit _stressUnit = DefaultUnits.StressUnitResult;
+    private bool _undefinedModelLengthUnit;
 
-    private ConcurrentDictionary<int, DiagramLine> _cachedLines;
+    private ConcurrentBag<DiagramLine> _cachedLines;
 
     public Elem1dResultDiagram() : base("1D Element Result Diagram", "ResultElem1dDiagram",
       "Displays GSA 1D Element Result Diagram", CategoryName.Name(), SubCategoryName.Cat5()) { }
@@ -63,7 +84,7 @@ namespace GsaGH.Components {
         return;
       }
 
-      foreach (DiagramLine diagramLine in _cachedLines.Values) {
+      foreach (DiagramLine diagramLine in _cachedLines) {
         args.Display.DrawLine(diagramLine.Line, diagramLine.Color);
       }
     }
@@ -74,6 +95,14 @@ namespace GsaGH.Components {
        .Where(item => item.Description == reader.GetString("DiagramType"))
        .Select(item => item.GsaGhEnum).FirstOrDefault();
       _defScale = reader.GetDouble("scale");
+      _lengthUnit = (LengthUnit)UnitsHelper.Parse(typeof(LengthUnit), reader.GetString("model"));
+      _lengthResultUnit
+        = (LengthUnit)UnitsHelper.Parse(typeof(LengthUnit), reader.GetString("length"));
+      _forceUnit = (ForceUnit)UnitsHelper.Parse(typeof(ForceUnit), reader.GetString("force"));
+      _momentUnit = (MomentUnit)UnitsHelper.Parse(typeof(MomentUnit), reader.GetString("moment"));
+      _stressUnit
+        = (PressureUnit)UnitsHelper.Parse(typeof(PressureUnit), reader.GetString("stress"));
+
       return base.Read(reader);
     }
 
@@ -88,7 +117,101 @@ namespace GsaGH.Components {
         Mappings.diagramTypeMapping.Where(item => item.GsaGhEnum == _displayedDiagramType)
          .Select(item => item.Description).FirstOrDefault());
       writer.SetDouble("scale", _defScale);
+      writer.SetString("model", Length.GetAbbreviation(_lengthUnit));
+      writer.SetString("length", Length.GetAbbreviation(_lengthResultUnit));
+      writer.SetString("force", Force.GetAbbreviation(_forceUnit));
+      writer.SetString("moment", Moment.GetAbbreviation(_momentUnit));
+      writer.SetString("stress", Pressure.GetAbbreviation(_stressUnit));
+
       return base.Write(writer);
+    }
+
+    protected override void AppendAdditionalComponentMenuItems(ToolStripDropDown menu) {
+      if (!(menu is ContextMenuStrip)) {
+        return; // this method is also called when clicking EWR balloon
+      }
+
+      Menu_AppendSeparator(menu);
+
+      var lengthUnitsMenu = new ToolStripMenuItem("Displacement") {
+        Enabled = true,
+      };
+      foreach (string unit in UnitsHelper.GetFilteredAbbreviations(EngineeringUnits.Length)) {
+        var toolStripMenuItem = new ToolStripMenuItem(unit, null, (s, e) => UpdateLength(unit)) {
+          Checked = unit == Length.GetAbbreviation(_lengthResultUnit),
+          Enabled = true,
+        };
+        lengthUnitsMenu.DropDownItems.Add(toolStripMenuItem);
+      }
+
+      var forceUnitsMenu = new ToolStripMenuItem("Force") {
+        Enabled = true,
+      };
+      foreach (string unit in UnitsHelper.GetFilteredAbbreviations(EngineeringUnits.Force)) {
+        var toolStripMenuItem = new ToolStripMenuItem(unit, null, (s, e) => UpdateForce(unit)) {
+          Checked = unit == Force.GetAbbreviation(_forceUnit),
+          Enabled = true,
+        };
+        forceUnitsMenu.DropDownItems.Add(toolStripMenuItem);
+      }
+
+      var momentUnitsMenu = new ToolStripMenuItem("Moment") {
+        Enabled = true,
+      };
+      foreach (string unit in UnitsHelper.GetFilteredAbbreviations(EngineeringUnits.Moment)) {
+        var toolStripMenuItem = new ToolStripMenuItem(unit, null, (s, e) => UpdateMoment(unit)) {
+          Checked = unit == Pressure.GetAbbreviation(_stressUnit),
+          Enabled = true,
+        };
+        momentUnitsMenu.DropDownItems.Add(toolStripMenuItem);
+      }
+
+      var stressUnitsMenu = new ToolStripMenuItem("Stress") {
+        Enabled = true,
+      };
+      foreach (string unit in UnitsHelper.GetFilteredAbbreviations(EngineeringUnits.Stress)) {
+        var toolStripMenuItem = new ToolStripMenuItem(unit, null, (s, e) => UpdateStress(unit)) {
+          Checked = unit == Pressure.GetAbbreviation(_stressUnit),
+          Enabled = true,
+        };
+        stressUnitsMenu.DropDownItems.Add(toolStripMenuItem);
+      }
+
+      var unitsMenu = new ToolStripMenuItem("Select Units", Resources.Units);
+
+      if (_undefinedModelLengthUnit) {
+        var modelUnitsMenu = new ToolStripMenuItem("Model geometry") {
+          Enabled = true,
+        };
+        foreach (string unit in UnitsHelper.GetFilteredAbbreviations(EngineeringUnits.Length)) {
+          var toolStripMenuItem = new ToolStripMenuItem(unit, null, (s, e) => UpdateModel(unit)) {
+            Checked = unit == Length.GetAbbreviation(_lengthUnit),
+            Enabled = true,
+          };
+          modelUnitsMenu.DropDownItems.Add(toolStripMenuItem);
+        }
+
+        unitsMenu.DropDownItems.AddRange(new ToolStripItem[] {
+          modelUnitsMenu,
+          lengthUnitsMenu,
+          forceUnitsMenu,
+          stressUnitsMenu,
+          momentUnitsMenu,
+        });
+      } else {
+        unitsMenu.DropDownItems.AddRange(new ToolStripItem[] {
+          lengthUnitsMenu,
+          forceUnitsMenu,
+          momentUnitsMenu,
+          stressUnitsMenu,
+        });
+      }
+
+      unitsMenu.ImageScaling = ToolStripItemImageScaling.SizeToFit;
+
+      menu.Items.Add(unitsMenu);
+
+      Menu_AppendSeparator(menu);
     }
 
     protected override void InitialiseDropdowns() {
@@ -160,39 +283,47 @@ namespace GsaGH.Components {
       }
 
       string elementlist = GetNodeFilters(da);
+
       var ghScale = new GH_Number();
       double scale = 1;
-      if (da.GetData(2, ref ghScale)) {
+      bool isNormalised = !da.GetData(2, ref ghScale);
+      if (!isNormalised) {
         GH_Convert.ToDouble(ghScale, out scale, GH_Conversion.Both);
       }
 
+      LengthUnit lengthUnit = result.Model.ModelUnit;
+      _undefinedModelLengthUnit = false;
+      if (lengthUnit == LengthUnit.Undefined) {
+        lengthUnit = _lengthUnit;
+        _undefinedModelLengthUnit = true;
+        this.AddRuntimeRemark(
+          "Model came straight out of GSA and we couldn't read the units. The geometry has been scaled to be in "
+          + lengthUnit + ". This can be changed by right-clicking the component -> 'Select Units'");
+      }
+
+      double computedScale = ComputeScale(lengthUnit, scale);
       var graphic = new GraphicSpecification() {
         Elements = elementlist,
         Type = Mappings.diagramTypeMapping.Where(item => item.GsaGhEnum == _displayedDiagramType)
          .Select(item => item.GsaApiEnum).FirstOrDefault(),
         Cases = _case,
-        IsNormalised
-          = true, // want to normalise it always! because for scaling stuff will look much better - that's why scaleFactor is outside object initialisation
+        ScaleFactor = computedScale,
+        IsNormalised = isNormalised,
       };
 
-      _cachedLines = new ConcurrentDictionary<int, DiagramLine>();
-      graphic.ScaleFactor = scale;
-      var linesFromModel = result.Model.Model.Draw(graphic).Lines.ToList();
+      _cachedLines = new ConcurrentBag<DiagramLine>();
+      ReadOnlyCollection<GsaAPI.Line> linesFromModel = result.Model.Model.Draw(graphic).Lines;
 
-      int i = 0;
       foreach (GsaAPI.Line item in linesFromModel) {
-        var line = new Line(item.Start.X, item.Start.Y, item.Start.Z, item.End.X, item.End.Y,
-          item.End.Z);
-        _cachedLines.TryAdd(i++, new DiagramLine(line, (Color)item.Color));
+        double lengthScaleFactor = UnitConverter.Convert(1, Length.BaseUnit, lengthUnit);
+        var startPoint = new Point3d(item.Start.X, item.Start.Y, item.Start.Z);
+        var endPoint = new Point3d(item.End.X, item.End.Y, item.End.Z);
+        startPoint *= lengthScaleFactor;
+        endPoint *= lengthScaleFactor;
+        _cachedLines.Add(new DiagramLine(startPoint, endPoint, (Color)item.Color));
       }
 
-      var lines = new List<Line>();
-
-      foreach (KeyValuePair<int, DiagramLine> keyValuePair in _cachedLines) {
-        lines.Add(keyValuePair.Value.Line);
-      }
-
-      da.SetData(1, lines);
+      da.SetData(1, _cachedLines);
 
       //PostHog.Result(result.Type, 1, resultType, _displayedDiagramType.ToString());
     }
@@ -225,5 +356,68 @@ namespace GsaGH.Components {
       return nodeList;
     }
 
+    private double ComputeScale(LengthUnit modelLengthUnit, double scaleInput) {
+      double diagramScaleFactor = 1.0d;
+      switch (_displayedDiagramType) {
+        case DiagramType.AxialForceFx:
+        case DiagramType.ShearForceFy:
+        case DiagramType.ShearForceFz:
+          diagramScaleFactor = UnitConverter.Convert(1, Force.BaseUnit, _forceUnit);
+          break;
+
+        case DiagramType.AxialStressA:
+        case DiagramType.BendingStressByNegativeZ:
+        case DiagramType.BendingStressByPositiveZ:
+        case DiagramType.BendingStressBzNegativeY:
+        case DiagramType.BendingStressBzPositiveY:
+        case DiagramType.CombinedStressC1:
+        case DiagramType.CombinedStressC2:
+        case DiagramType.ShearStressSz:
+        case DiagramType.ShearStressSy:
+          diagramScaleFactor = UnitConverter.Convert(1, Pressure.BaseUnit, _stressUnit);
+
+          break;
+        case DiagramType.MomentMyy:
+        case DiagramType.MomentMzz:
+        case DiagramType.ResolvedMomentMyz:
+        case DiagramType.TorsionMxx:
+          diagramScaleFactor = UnitConverter.Convert(1, Moment.BaseUnit, _momentUnit);
+
+          break;
+      }
+
+      double lengthScaleFactor = UnitConverter.Convert(1, Length.BaseUnit, modelLengthUnit);
+      return scaleInput * diagramScaleFactor * lengthScaleFactor;
+    }
+
+    private void UpdateForce(string unit) {
+      _forceUnit = (ForceUnit)UnitsHelper.Parse(typeof(ForceUnit), unit);
+      ExpirePreview(true);
+      base.UpdateUI();
+    }
+
+    private void UpdateStress(string unit) {
+      _stressUnit = (PressureUnit)UnitsHelper.Parse(typeof(PressureUnit), unit);
+      ExpirePreview(true);
+      base.UpdateUI();
+    }
+
+    private void UpdateLength(string unit) {
+      _lengthResultUnit = (LengthUnit)UnitsHelper.Parse(typeof(LengthUnit), unit);
+      ExpirePreview(true);
+      base.UpdateUI();
+    }
+
+    private void UpdateModel(string unit) {
+      _lengthUnit = (LengthUnit)UnitsHelper.Parse(typeof(LengthUnit), unit);
+      ExpirePreview(true);
+      base.UpdateUI();
+    }
+
+    private void UpdateMoment(string unit) {
+      _momentUnit = (MomentUnit)UnitsHelper.Parse(typeof(MomentUnit), unit);
+      ExpirePreview(true);
+      base.UpdateUI();
+    }
   }
 }
