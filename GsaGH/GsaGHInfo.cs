40edfc6feb833f8edab85524b05d81ec342fae93
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-05-12 18:15:22 +0200

    move solver dll check to own class

--- GsaGH/GsaGHInfo.cs
+++ GsaGH/GsaGHInfo.cs
@@ -171,37 +171,6 @@ namespace GsaGH {
     }
   }
 
-  public static class SolverRequiredDll {
-    internal static string LoadedFromPath { get; private set; }
-    private static bool canAnalyse;
-    private static bool loaded;
-
-    public static bool IsCorrectVersionLoaded() {
-      if (!loaded || !canAnalyse) {
-        ProcessModuleCollection dlls = Process.GetCurrentProcess().Modules;
-        foreach (ProcessModule module in dlls) {
-          if (module.ModuleName != "libiomp5md.dll") {
-            continue;
-          }
-
-          loaded = true;
-          string gsaVersion = FileVersionInfo
-           .GetVersionInfo(AddReferencePriority.InstallPath + "\\libiomp5md.dll").FileVersion;
-          if (FileVersionInfo.GetVersionInfo(module.FileName).FileVersion == gsaVersion) {
-            canAnalyse = true;
-          } else {
-            canAnalyse = false;
-            LoadedFromPath = module.FileName;
-          }
-
-          break;
-        }
-      }
-
-      return !loaded || canAnalyse;
-    }
-  }
-
   internal sealed class PluginInfo {
     public static OasysPluginInfo Instance => lazy.Value;
     private static readonly Lazy<OasysPluginInfo> lazy = new Lazy<OasysPluginInfo>(()
