bd15dc8834e62bef9660b3b47cd2c9c3cd0653b2
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-05-25 16:44:06 +0200

    cast caseid to int

--- GsaGH/Parameters/5_Results/GsaResultGoo.cs
+++ GsaGH/Parameters/5_Results/GsaResultGoo.cs
@@ -1,4 +1,5 @@
-﻿using Grasshopper.Kernel.Types;
+﻿using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
 using OasysGH;
 using OasysGH.Parameters;
 
@@ -13,6 +14,30 @@ namespace GsaGH.Parameters {
     public override OasysPluginInfo PluginInfo => GsaGH.PluginInfo.Instance;
 
     public GsaResultGoo(GsaResult item) : base(item) { }
+
+    public override bool CastTo<TQ>(ref TQ target) {
+      if (base.CastTo(ref target)) {
+        return true;
+      }
+
+      if (typeof(TQ).IsAssignableFrom(typeof(GH_Integer))) {
+        if (Value == null) {
+          target = default;
+        } else {
+          var ghint = new GH_Integer();
+          if (GH_Convert.ToGHInteger(Value.CaseId, GH_Conversion.Both, ref ghint)) {
+            target = (TQ)(object)ghint;
+          } else {
+            target = default;
+          }
+        }
+
+        return true;
+      }
+
+      target = default;
+      return false;
+    }
 
     public override IGH_Goo Duplicate() {
       return this;
