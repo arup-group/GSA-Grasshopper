363045a0fc5badf2f74764cdfd98fec3f6cde7df
DominikaLos <dominika.los@arup.com>
2023-06-01 10:43:49 +0200

    gsagh-293 reworking AnnotationGoo

--- GsaGH/Parameters/5_Results/AnnotationGoo.cs
+++ GsaGH/Parameters/5_Results/AnnotationGoo.cs
@@ -1,54 +1,58 @@
-﻿using Grasshopper.Kernel;
+﻿using System.Drawing;
+using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
+using OasysGH;
+using OasysGH.Parameters;
+using OasysGH.Units;
 using Rhino.Geometry;
-using System.Drawing;
 
 namespace GsaGH.Parameters {
-  internal class AnnotationGoo : GH_GeometricGoo<Point3d>, IGH_PreviewData {
+  internal class AnnotationGoo : GH_OasysGeometricGoo<TextDot>, IGH_PreviewData {
     public override string TypeName => "Annotation";
+    public override OasysPluginInfo PluginInfo => GsaGH.PluginInfo.Instance;
     public override string TypeDescription => "Annotation for diagram result";
-    public override BoundingBox Boundingbox {
-      get {
-        var box = new BoundingBox(Value, Value);
-        box.Inflate(1);
-        return box;
-      }
-    }
-
-    public void DrawViewportWires(GH_PreviewWireArgs args) {
-      args.Pipeline.Draw2dText(Text, Color, new Point3d(Position.X, Position.Y, Position.Z), true);
-    }
-
-    public void DrawViewportMeshes(GH_PreviewMeshArgs args) {
-    }
-
-    public BoundingBox ClippingBox => Boundingbox;
 
     public Vector3d Position { get; private set; }
     public Color Color { get; private set; }
     public string Text { get; private set; }
 
-    public AnnotationGoo(Vector3d position, Color color, string text) {
+    public AnnotationGoo(Vector3d position, Color color, string text) : base(new TextDot(text,
+      new Point3d(position))) {
       Position = position;
       Color = color;
       Text = text;
     }
 
-    public override string ToString() { return $"Position {Position}, Value: {Text}"; }
-    public override IGH_GeometricGoo DuplicateGeometry() { return new AnnotationGoo(Position, Color, Text); }
+    public override void DrawViewportWires(GH_PreviewWireArgs args) {
+      var coordinate = new Point3d(Position.X, Position.Y, Position.Z);
+      args.Pipeline.Draw2dText(Text, Color, coordinate, true);
+    }
 
-    public override BoundingBox GetBoundingBox(Transform xform) {
-      Point3d point = Value;
-      point.Transform(xform);
-      var box = new BoundingBox(point, point);
-      box.Inflate(1);
-      return box;
+    public override void DrawViewportMeshes(GH_PreviewMeshArgs args) { }
+
+    public override IGH_GeometricGoo Duplicate() {
+      return new AnnotationGoo(Position, Color, Text);
+    }
+
+    public override string ToString() { return $"Position {Position}, Value: {Text}"; }
+
+    public override GeometryBase GetGeometry() {
+      if (Value?.Point == null) {
+        return null;
+      }
+
+      Point3d pt1 = Value.Point;
+      pt1.Z += DefaultUnits.Tolerance.As(DefaultUnits.LengthUnitGeometry) / 2;
+      Point3d pt2 = Value.Point;
+      pt2.Z += DefaultUnits.Tolerance.As(DefaultUnits.LengthUnitGeometry) / -2;
+      var ln = new Line(pt1, pt2);
+      return new LineCurve(ln);
     }
 
     public override IGH_GeometricGoo Transform(Transform xform) {
-      Point3d point = Value;
-      point.Transform(xform);
-      return new AnnotationGoo(new Vector3d(point), Color, Text);
+      TextDot value = Value;
+      value.Transform(xform);
+      return new AnnotationGoo(new Vector3d(value.Point), Color, Text);
     }
 
     public override IGH_GeometricGoo Morph(SpaceMorph xmorph) {
