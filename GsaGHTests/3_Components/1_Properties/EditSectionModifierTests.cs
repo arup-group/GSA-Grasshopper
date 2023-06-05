852edf00b0dbacd4b3635423b727f109abd70443
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-06-05 11:06:30 +0200

    #GSA-6036

--- GsaGHTests/3_Components/1_Properties/EditSectionModifierTests.cs
+++ GsaGHTests/3_Components/1_Properties/EditSectionModifierTests.cs
@@ -29,6 +29,7 @@ namespace GsaGHTests.Properties {
         K22Modifier = new Ratio(0.1, RatioUnit.DecimalFraction),
         VolumeModifier = new Ratio(0.8, RatioUnit.DecimalFraction),
         AdditionalMass = new LinearDensity(5, LinearDensityUnit.KilogramPerMeter),
+        IsBendingAxesPrincipal = true,
         IsReferencePointCentroid = false,
         StressOption = GsaSectionModifier.StressOptionType.UseModified,
       };
@@ -46,7 +47,7 @@ namespace GsaGHTests.Properties {
       var k22Out = (GH_UnitNumber)ComponentTestHelper.GetOutput(comp, i++);
       var volOut = (GH_UnitNumber)ComponentTestHelper.GetOutput(comp, i++);
       var addMassOut = (GH_UnitNumber)ComponentTestHelper.GetOutput(comp, i++);
-      i++; //var isBendingOut = (GH_Boolean)ComponentTestHelper.GetOutput(comp, i++);
+      var isBendingOut = (GH_Boolean)ComponentTestHelper.GetOutput(comp, i++);
       var isRefPtOut = (GH_Boolean)ComponentTestHelper.GetOutput(comp, i++);
       var stressOptOut = (GH_Integer)ComponentTestHelper.GetOutput(comp, i++);
 
@@ -60,7 +61,7 @@ namespace GsaGHTests.Properties {
       Assert.Equal(0.1, k22Out.Value.As(RatioUnit.DecimalFraction), 6);
       Assert.Equal(0.8, volOut.Value.As(RatioUnit.DecimalFraction), 6);
       Assert.Equal(5, addMassOut.Value.As(LinearDensityUnit.KilogramPerMeter), 6);
-      //Assert.True(isBending_out.Value); TO-DO GSA-6036
+      Assert.True(isBendingOut.Value); 
       Assert.False(isRefPtOut.Value);
       Assert.Equal(1, stressOptOut.Value);
       Assert.True(modifierdGoo.Value.IsModified);
