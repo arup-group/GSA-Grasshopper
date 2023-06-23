094384bbae1ed19322945a605f268bdf49df01b6
Kristjan Nielsen <kpn@kpne.dk>
2023-06-23 10:38:48 +0200

    GsaGH-306 GetMaterials component (#424)

--- GsaGH/Properties/Resources.Designer.cs
+++ GsaGH/Properties/Resources.Designer.cs
@@ -793,6 +793,16 @@ namespace GsaGH.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap GetMaterials {
+            get {
+                object obj = ResourceManager.GetObject("GetMaterials", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap GetResults {
             get {
                 object obj = ResourceManager.GetObject("GetResults", resourceCulture);
