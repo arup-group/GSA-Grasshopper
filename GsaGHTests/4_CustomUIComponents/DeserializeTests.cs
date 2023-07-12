9e4e350ad67fef907f5681510b0beb0919cdb531
DominikaLos <dominika.los@arup.com>
2023-07-12 15:22:24 +0200

    refactor which should increase coverage

--- GsaGHTests/4_CustomUIComponents/DeserializeTests.cs
+++ GsaGHTests/4_CustomUIComponents/DeserializeTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using GsaGH.Components;
+using GsaGH.Components.GraveyardComp;
 using GsaGHTests.Helpers;
 using OasysGH.Components;
 using Xunit;
@@ -9,20 +10,75 @@ namespace GsaGHTests.CustomComponent {
   public class DeserializeTests {
 
     [Theory]
+    //Model
+    [InlineData(typeof(CreateModel))]
     [InlineData(typeof(OpenModel))]
+    [InlineData(typeof(SaveModel))]
+    //Properties
     [InlineData(typeof(CreateBool6))]
+    [InlineData(typeof(CreateProp2dModifier))]
+    [InlineData(typeof(CreateSectionModifier))]
     [InlineData(typeof(EditOffset))]
     [InlineData(typeof(EditProp2d))]
-    [InlineData(typeof(GetProp2dModifier))]
-    [InlineData(typeof(GetSectionModifier))]
-    [InlineData(typeof(GetSectionDimensions))]
-    [InlineData(typeof(GetSectionProperties))]
     [InlineData(typeof(GetMaterialProperties))]
+    [InlineData(typeof(GetProp2dModifier))]
+    [InlineData(typeof(GetSectionDimensions))]
+    [InlineData(typeof(GetSectionModifier))]
+    [InlineData(typeof(GetSectionProperties))]
+    //Geometry
     [InlineData(typeof(CreateMember1d))]
     [InlineData(typeof(CreateSupport))]
     [InlineData(typeof(EditMember1d))]
     [InlineData(typeof(EditNode))]
+    [InlineData(typeof(Element2dFromBrep))]
+    [InlineData(typeof(ElementFromMembers))]
+    //Loads
+    [InlineData(typeof(CreateGridAreaLoad))]
+    [InlineData(typeof(CreateGridLineLoad))]
+    [InlineData(typeof(CreateGridPlane))]
+    [InlineData(typeof(CreateGridPointLoad))]
+    [InlineData(typeof(CreateGridSurface))]
     [InlineData(typeof(GridPlaneSurfaceProperties))]
+    //Analysis
+    [InlineData(typeof(GhAnalyse))]
+    //Results
+    [InlineData(typeof(BeamStrainEnergy))]
+    [InlineData(typeof(Elem1dContourResults))]
+    [InlineData(typeof(Elem1dResultDiagram))]
+    [InlineData(typeof(Elem2dContourResults))]
+    [InlineData(typeof(Elem3dContourResults))]
+    [InlineData(typeof(NodeContourResults))]
+    [InlineData(typeof(ReactionForceDiagrams))]
+    //Graveyard
+    [InlineData(typeof(CreateBool6_OBSOLETE))]
+    [InlineData(typeof(CreateGridPlane_OBSOLETE))]
+    [InlineData(typeof(CreateGridSurface_OBSOLETE))]
+    [InlineData(typeof(CreateMaterial_OBSOLETE))]
+    [InlineData(typeof(CreateMember1d_OBSOLETE))]
+    [InlineData(typeof(CreateMember2d_OBSOLETE))]
+    [InlineData(typeof(CreateProfile_OBSOLETE))]
+    [InlineData(typeof(CreateProp2d_OBSOLETE))]
+    [InlineData(typeof(EditMember1d3_OBSOLETE))]
+    [InlineData(typeof(EditMember1d_OBSOLETE))]
+    [InlineData(typeof(EditMember2d_OBSOLETE))]
+    [InlineData(typeof(EditMember3d_OBSOLETE))]
+    [InlineData(typeof(EditNode_OBSOLETE))]
+    [InlineData(typeof(EditOffset_OBSOLETE))]
+    [InlineData(typeof(EditProp2d3_OBSOLETE))]
+    [InlineData(typeof(EditProp2d4_OBSOLETE))]
+    [InlineData(typeof(EditProp2d5_OBSOLETE))]
+    [InlineData(typeof(EditProp2d6_OBSOLETE))]
+    [InlineData(typeof(EditProp2d_OBSOLETE))]
+    [InlineData(typeof(EditSectionModifier2_OBSOLETE))]
+    [InlineData(typeof(EditSectionModifier_OBSOLETE))]
+    [InlineData(typeof(Elem1dContourResults_OBSOLETE))]
+    [InlineData(typeof(Elem2dContourResults_OBSOLETE))]
+    [InlineData(typeof(Elem2dFromBrep2_OBSOLETE))]
+    [InlineData(typeof(GetSectionProperties2_OBSOLETE))]
+    [InlineData(typeof(GetSectionProperties_OBSOLETE))]
+    [InlineData(typeof(NodeContourResults_OBSOLETE))]
+    [InlineData(typeof(ReactionForceDiagrams2_OBSOLETE))]
+    [InlineData(typeof(ReactionForceDiagrams_OBSOLETE))]
     public void DeSerializeComponentTest(Type t) {
       var comp = (GH_OasysComponent)Activator.CreateInstance(t);
       OasysDropDownComponentTestHelper.TestDeserialize(comp);
---
+++ GsaGHTests/Helpers/GenerateToolStripMenuItemTest.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows.Forms;
+using GsaGH.Helpers.GH;
+using OasysGH.Units.Helpers;
+using Xunit;
+
+namespace GsaGHTests.Helpers {
+  [Collection("GrasshopperFixture collection")]
+  public class GenerateToolStripMenuItemTest {
+    [Fact]
+    public void WhenGenerateToolStripMenuItemWithEmptyStrings() {
+      ToolStripMenuItem obj = GenerateToolStripMenuItem.GetSubMenuItem(string.Empty,
+        EngineeringUnits.Acceleration, string.Empty, s => { });
+
+      Assert.Null(obj);
+    }
+
+    [Fact]
+    public void WhenGenerateToolStripMenuItem() {
+      ToolStripMenuItem obj = GenerateToolStripMenuItem.GetSubMenuItem("Force",
+        EngineeringUnits.Force, "kN", s => { });
+
+      Assert.Equal("Force", obj.Text);
+      Assert.True(obj.Enabled);
+      Assert.False(obj.Checked);
+      Assert.True(obj.DropDownItems[1].Text == "kN");
+      Assert.True(6 == obj.DropDownItems.Count);
+
+      obj.Checked = true;
+      obj.Enabled = false;
+      Assert.True(obj.Checked);
+      Assert.False(obj.Enabled);
+    }
+  }
+}
