8ebc6591e3e6b81661acab69ff72dc0278fb7342
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-07-17 12:18:02 +0200

    GsaGhInfo and UI tests

---
+++ GsaGHTests/UI/LoadMainMenuTests.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+using GsaGH.Graphics.Menu;
+using Xunit;
+
+namespace GsaGHTests.UI {
+  [Collection("GrasshopperFixture collection")]
+  public class LoadMainMenuTests {
+    [Fact]
+    public void LoadMainMenuTest() {
+      var oasysMenu = new ToolStripMenuItem("Oasys");
+      MenuLoad.PopulateSub(oasysMenu);
+      Assert.Equal(3, oasysMenu.DropDown.Items.Count);
+    }
+  }
+}
