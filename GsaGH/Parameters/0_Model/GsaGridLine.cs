bfa31de297ef4bc77e0e2f5ef98aa8081552d106
Tilman Reinhardt <tilman.reinhardt@arup.com>
2023-08-11 12:11:37 +0200

    GSAGH-139 grid line parameter classes

---
+++ GsaGH/Parameters/0_Model/GsaGridLine.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using GsaAPI;
+using GsaGH.Helpers.Import;
+using LengthUnit = OasysUnits.Units.LengthUnit;
+
+namespace GsaGH.Parameters {
+  public class GsaGridLine {
+    private GridLine _gridLine;
+
+    public GsaGridLine() { }
+
+    internal GsaGridLine(GridLine gridLine) {
+      _gridLine = gridLine;
+    }
+
+    //internal EntityList GetApiList() {
+    //  return new EntityList {
+    //    Name = Name,
+    //    Definition = Definition,
+    //    Type = GetAPIEntityType(EntityType)
+    //  };
+    //}
+
+    public GsaGridLine Duplicate() {
+      var dup = new GsaGridLine {
+        _gridLine = _gridLine
+      };
+
+      return dup;
+    }
+
+    public override string ToString() {
+      return "";
+    }
+  }
+}
---
+++ GsaGH/Parameters/0_Model/GsaGridLineParameter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+using GsaGH.Helpers.GH;
+using GsaGH.Properties;
+using OasysGH.Parameters;
+
+namespace GsaGH.Parameters {
+  /// <summary>
+  /// This class provides a parameter interface for the <see cref="GsaGridLineGoo" /> type.
+  /// </summary>
+  public class GsaGridLineParameter : GH_OasysPersistentParam<GsaGridLineGoo> {
+    public override Guid ComponentGuid => new Guid("e5e50621-12c9-4ac4-abb0-926d60414ea7");
+    public override GH_Exposure Exposure => GH_Exposure.primary | GH_Exposure.obscure;
+    public override string InstanceDescription
+      => m_data.DataCount == 0 ? "Empty " + GsaGridLineGoo.Name + " parameter" :
+        base.InstanceDescription;
+    public override string TypeName => SourceCount == 0 ? GsaGridLineGoo.Name : base.TypeName;
+    protected override Bitmap Icon => Resources.ListParam;
+
+    public GsaGridLineParameter() : base(new GH_InstanceDescription(GsaGridLineGoo.Name,
+      GsaGridLineGoo.NickName, GsaGridLineGoo.Description + " parameter", CategoryName.Name(),
+      SubCategoryName.Cat9())) { }
+
+    protected override GsaGridLineGoo PreferredCast(object data) {
+      this.AddRuntimeError($"Data conversion failed from {data.GetTypeName()} to Grid Line");
+      return new GsaGridLineGoo(null);
+    }
+  }
+}
