dbfd3a28997aa1387066ea625e3641aa0f76eb29
Tilman Reinhardt <tilman.reinhardt@arup.com>
2023-08-24 12:43:19 +0200

    GSAGH-139 draw grid line nicely

--- GsaGH/Parameters/0_Model/GsaGridLineGoo.cs
+++ GsaGH/Parameters/0_Model/GsaGridLineGoo.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Drawing;
-using System.Security.Cryptography;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Types;
 using OasysGH;
 using OasysGH.Parameters;
+using OasysGH.Units;
+using OasysUnits.Units;
+using Rhino.Display;
+using Rhino.DocObjects;
 using Rhino.Geometry;
 
 namespace GsaGH.Parameters {
@@ -51,33 +54,62 @@ namespace GsaGH.Parameters {
     }
 
     public override void DrawViewportMeshes(GH_PreviewMeshArgs args) {
-      //if (Value == null) {
-      //  return;
-      //}
-
-      Color color = Color.Black;
-      int pattern = 999999;
-      if(Value._pattern != 0) {
-        pattern = Value._pattern;
+      // we need to scale grid lines according to the users unit settings
+      double unitLength = 1;
+      LengthUnit _lengthUnit = DefaultUnits.LengthUnitGeometry;
+      switch (DefaultUnits.LengthUnitGeometry) {
+        case LengthUnit.Millimeter:
+          unitLength = 1000;
+          break;
+        case LengthUnit.Centimeter:
+          unitLength = 100;
+          break;
+        case LengthUnit.Inch:
+          unitLength = 39.3701;
+          break;
+        case LengthUnit.Foot:
+          unitLength = 3.28084;
+          break;
       }
-      int thickness = 1;
+
       Point3d[] points;
       if (Value._curve.IsLinear()) {
         points = new Point3d[2] { Value._curve.SegmentCurve(0).PointAtStart, Value._curve.SegmentCurve(0).PointAtEnd };
-      } else {
 
-        points = Value._curve.SegmentCurve(0).DivideEquidistant(Value._curve.SegmentCurve(0).GetLength() / 360);
+      } else {
+        points = Value._curve.SegmentCurve(0).DivideEquidistant(Value._curve.SegmentCurve(0).GetLength() / 360.0);
       }
       if (points != null) {
+        int thickness = 1;
+        Color color = Color.Black;
+        int pattern = 999999;
+        if (Value._pattern != 0) {
+          pattern = Value._pattern;
+        }
         args.Pipeline.DrawPatternedPolyline(points, color, pattern, thickness, false);
+
+        double radius = 0.618046972 * unitLength; // in golden ratio
+        Plane plane = Plane.WorldXY;
+        Point3d origin = points[0];
+        Vector3d distance = Value._curve.TangentAtStart;
+        distance.Unitize();
+        distance *= -radius;
+        origin.Transform(Rhino.Geometry.Transform.Translation(distance));
+        plane.Origin = origin;
+
+        // we need to shorten text if it´s too long
+        var text = new Text3d(Value._gridLine.Label.Substring(0, 4), plane, 0.381982059 * unitLength) { // golden ratio
+          HorizontalAlignment = TextHorizontalAlignment.Center,
+          VerticalAlignment = TextVerticalAlignment.Middle
+        };
+        args.Pipeline.Draw3dText(text, color);
+
+        var circle = new Circle(plane, radius);
+        args.Pipeline.DrawCircle(circle, color);
       }
     }
 
     public override void DrawViewportWires(GH_PreviewWireArgs args) {
-      if (Value == null) {
-        return;
-      }
-
 
     }
 
