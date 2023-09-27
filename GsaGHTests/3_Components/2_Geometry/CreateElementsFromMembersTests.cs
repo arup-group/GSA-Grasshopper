254a3e8ca7bd11fd260b6a1752801ed187f6eef7
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-09-27 15:42:54 +0200

    CreateElementsFromMembersTests

---
+++ GsaGHTests/3_Components/2_Geometry/CreateElementsFromMembersTests.cs
@@ -0,0 +1,72 @@
+﻿using Grasshopper.Kernel.Types;
+using GsaAPI;
+using GsaGH.Components;
+using GsaGH.Parameters;
+using GsaGHTests.Components.Properties;
+using GsaGHTests.Helpers;
+using OasysGH.Components;
+using OasysUnits;
+using Rhino.Geometry;
+using System.Linq;
+using Xunit;
+using LengthUnit = OasysUnits.Units.LengthUnit;
+
+namespace GsaGHTests.Components.Geometry {
+  [Collection("GrasshopperFixture collection")]
+  public class CreateElementsFromMembersTests {
+
+    public static GH_OasysComponent ComponentMother() {
+      var comp = new CreateElementsFromMembers();
+      comp.CreateAttributes();
+      ComponentTestHelper.SetInput(comp, (GsaNodeGoo)ComponentTestHelper.GetOutput(
+        CreateSupportTests.ComponentMother()), 0);
+      ComponentTestHelper.SetInput(comp, (GsaMember1dGoo)ComponentTestHelper.GetOutput(
+        CreateMember1dTests.ComponentMother()), 1);
+      ComponentTestHelper.SetInput(comp, (GsaMember2dGoo)ComponentTestHelper.GetOutput(
+        CreateMember2dTests.ComponentMother()), 2);
+      ComponentTestHelper.SetInput(comp, (GsaMember3dGoo)ComponentTestHelper.GetOutput(
+        CreateMember3dTests.ComponentMother()), 3);
+      return comp;
+    }
+
+    [Fact]
+    public void CreateComponentTest() {
+      GH_OasysComponent comp = ComponentMother();
+      var node = (GsaNodeGoo)ComponentTestHelper.GetOutput(comp, 0);
+      var e1d = (GsaElement1dGoo)ComponentTestHelper.GetOutput(comp, 1);
+      var e2d = (GsaElement2dGoo)ComponentTestHelper.GetOutput(comp, 2);
+      var e3d = (GsaElement3dGoo)ComponentTestHelper.GetOutput(comp, 3);
+
+      Assert.NotNull(node);
+      Assert.Equal(1, node.Value.Id);
+      Assert.Equal(0, node.Value.Point.X);
+      Assert.Equal(-1, node.Value.Point.Y);
+      Assert.Equal(0, node.Value.Point.Z);
+
+      Assert.NotNull(e1d);
+      Assert.Equal(1, e1d.Value.Id);
+      Assert.Equal(0, e1d.Value.Line.PointAtStart.X);
+      Assert.Equal(-1, e1d.Value.Line.PointAtStart.Y);
+      Assert.Equal(0, e1d.Value.Line.PointAtStart.Z);
+
+      Assert.NotNull(e2d);
+      Assert.Equal(400, e2d.Value.Ids.Count);
+      Assert.Equal(400, e2d.Value.Mesh.Faces.Count);
+
+      Assert.NotNull(e3d);
+      Assert.Equal(8000, e3d.Value.Ids.Count);
+      Assert.Equal(48000, e3d.Value.NgonMesh.Faces.Count);
+    }
+
+    [Fact]
+    public void DrawViewportMeshesAndWiresTest() {
+      GH_OasysComponent comp = ComponentMother();
+      var node = (GsaNodeGoo)ComponentTestHelper.GetOutput(comp, 0);
+      var e1d = (GsaElement1dGoo)ComponentTestHelper.GetOutput(comp, 1);
+      var e2d = (GsaElement2dGoo)ComponentTestHelper.GetOutput(comp, 2);
+      var e3d = (GsaElement3dGoo)ComponentTestHelper.GetOutput(comp, 3);
+
+      ComponentTestHelper.DrawViewportMeshesAndWiresTest(comp);
+    }
+  }
+}
