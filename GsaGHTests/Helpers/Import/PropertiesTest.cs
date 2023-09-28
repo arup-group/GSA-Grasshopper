22916e04e7c2888e5117783c6d34609d52ab7547
Tilman Reinhardt <tilman.reinhardt@arup.com>
2023-09-28 11:12:16 +0200

    GSAGH-364 fixed test

--- GsaGHTests/Helpers/Import/PropertiesTest.cs
+++ GsaGHTests/Helpers/Import/PropertiesTest.cs
@@ -1,5 +1,4 @@
 ﻿using GsaAPI;
-using GsaGH.Helpers.Import;
 using GsaGH.Parameters;
 using Xunit;
 
@@ -9,58 +8,54 @@ namespace GsaGHTests.Helpers.Import {
     [Fact]
     public void ImportSectionsTest() {
       GsaAPI.Model model = ImportPropertiesMotherModel();
-      var materials = new Materials(model);
-
       var gsaModel = new GsaModel(model);
 
       Assert.Equal("STD R 800 400", gsaModel.Sections[1].Value.ApiSection.Profile);
-      Duplicates.AreEqual(materials.SteelMaterials[1], gsaModel.Sections[1].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.SteelMaterials[1], gsaModel.Sections[1].Value.Material);
       Assert.Equal("STD R 800 400", gsaModel.Sections[2].Value.ApiSection.Profile);
-      Duplicates.AreEqual(materials.ConcreteMaterials[1], gsaModel.Sections[2].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.ConcreteMaterials[1], gsaModel.Sections[2].Value.Material);
       Assert.Equal("STD R 800 400", gsaModel.Sections[3].Value.ApiSection.Profile);
-      Duplicates.AreEqual(materials.FrpMaterials[1], gsaModel.Sections[3].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.FrpMaterials[1], gsaModel.Sections[3].Value.Material);
       Assert.Equal("STD R 800 400", gsaModel.Sections[4].Value.ApiSection.Profile);
-      Duplicates.AreEqual(materials.AluminiumMaterials[1], gsaModel.Sections[4].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.AluminiumMaterials[1], gsaModel.Sections[4].Value.Material);
       Assert.Equal("STD R 800 400", gsaModel.Sections[5].Value.ApiSection.Profile);
-      Duplicates.AreEqual(materials.TimberMaterials[1], gsaModel.Sections[5].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.TimberMaterials[1], gsaModel.Sections[5].Value.Material);
       Assert.Equal("STD R 800 400", gsaModel.Sections[6].Value.ApiSection.Profile);
-      Duplicates.AreEqual(materials.GlassMaterials[1], gsaModel.Sections[6].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.GlassMaterials[1], gsaModel.Sections[6].Value.Material);
     }
 
     [Fact]
     public void ImportProp2dsTest() {
       GsaAPI.Model model = ImportPropertiesMotherModel();
-      Materials materials = MaterialsTest.ImportMaterialsMother();
       var gsaModel = new GsaModel(model);
 
       Assert.Equal(200, gsaModel.Prop2ds[1].Value.Thickness.Millimeters);
-      Duplicates.AreEqual(materials.SteelMaterials[1], gsaModel.Prop2ds[1].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.SteelMaterials[1], gsaModel.Prop2ds[1].Value.Material);
       Assert.Equal(200, gsaModel.Prop2ds[2].Value.Thickness.Millimeters);
-      Duplicates.AreEqual(materials.ConcreteMaterials[1], gsaModel.Prop2ds[2].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.ConcreteMaterials[1], gsaModel.Prop2ds[2].Value.Material);
       Assert.Equal(200, gsaModel.Prop2ds[3].Value.Thickness.Millimeters);
-      Duplicates.AreEqual(materials.FrpMaterials[1], gsaModel.Prop2ds[3].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.FrpMaterials[1], gsaModel.Prop2ds[3].Value.Material);
       Assert.Equal(200, gsaModel.Prop2ds[4].Value.Thickness.Millimeters);
-      Duplicates.AreEqual(materials.AluminiumMaterials[1], gsaModel.Prop2ds[4].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.AluminiumMaterials[1], gsaModel.Prop2ds[4].Value.Material);
       Assert.Equal(200, gsaModel.Prop2ds[5].Value.Thickness.Millimeters);
-      Duplicates.AreEqual(materials.TimberMaterials[1], gsaModel.Prop2ds[5].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.TimberMaterials[1], gsaModel.Prop2ds[5].Value.Material);
       Assert.Equal(200, gsaModel.Prop2ds[6].Value.Thickness.Millimeters);
-      Duplicates.AreEqual(materials.GlassMaterials[1], gsaModel.Prop2ds[6].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.GlassMaterials[1], gsaModel.Prop2ds[6].Value.Material);
       Assert.Equal(200, gsaModel.Prop2ds[7].Value.Thickness.Millimeters);
-      Assert.Equal(materials.FabricMaterials[1].Name, gsaModel.Prop2ds[7].Value.Material.Name);
+      Assert.Equal(gsaModel.Materials.FabricMaterials[1].Name, gsaModel.Prop2ds[7].Value.Material.Name);
     }
 
     [Fact]
     public void ImportProp3dsTest() {
       GsaAPI.Model model = ImportPropertiesMotherModel();
-      Materials materials = MaterialsTest.ImportMaterialsMother();
       var gsaModel = new GsaModel(model);
 
-      Duplicates.AreEqual(materials.SteelMaterials[1], gsaModel.Prop3ds[1].Value.Material);
-      Duplicates.AreEqual(materials.ConcreteMaterials[1], gsaModel.Prop3ds[2].Value.Material);
-      Duplicates.AreEqual(materials.FrpMaterials[1], gsaModel.Prop3ds[3].Value.Material);
-      Duplicates.AreEqual(materials.AluminiumMaterials[1], gsaModel.Prop3ds[4].Value.Material);
-      Duplicates.AreEqual(materials.TimberMaterials[1], gsaModel.Prop3ds[5].Value.Material);
-      Duplicates.AreEqual(materials.GlassMaterials[1], gsaModel.Prop3ds[6].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.SteelMaterials[1], gsaModel.Prop3ds[1].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.ConcreteMaterials[1], gsaModel.Prop3ds[2].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.FrpMaterials[1], gsaModel.Prop3ds[3].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.AluminiumMaterials[1], gsaModel.Prop3ds[4].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.TimberMaterials[1], gsaModel.Prop3ds[5].Value.Material);
+      Duplicates.AreEqual(gsaModel.Materials.GlassMaterials[1], gsaModel.Prop3ds[6].Value.Material);
     }
 
     internal static GsaAPI.Model ImportPropertiesMotherModel() {
