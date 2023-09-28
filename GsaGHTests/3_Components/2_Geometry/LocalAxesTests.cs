5f48795873143646c94f46aa1722f5a5eea2994e
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-09-28 13:07:36 +0200

    LocalAxesTests

---
+++ GsaGHTests/3_Components/2_Geometry/LocalAxesTests.cs
@@ -0,0 +1,68 @@
+﻿using Grasshopper.Kernel.Types;
+using GsaGH.Components;
+using GsaGH.Parameters;
+using GsaGHTests.Helpers;
+using OasysGH.Components;
+using OasysUnits.Units;
+using Rhino.Geometry;
+using Xunit;
+using LocalAxes = GsaGH.Components.LocalAxes;
+
+namespace GsaGHTests.Components.Geometry {
+  [Collection("GrasshopperFixture collection")]
+  public class LocalAxesTests {
+    [Fact]
+    public void MemberLocalAxesTest() {
+      var comp = new LocalAxes();
+      comp.CreateAttributes();
+      var member = new GsaMember1d(new LineCurve(new Point3d(0, 0, 0), new Point3d(0, 0, 10)));
+      var goo = new GsaMember1dGoo(member);
+      ComponentTestHelper.SetInput(comp, goo);
+
+      var vectorX = (GH_Vector)ComponentTestHelper.GetOutput(comp, 0);
+      var vectorY = (GH_Vector)ComponentTestHelper.GetOutput(comp, 1);
+      var vectorZ = (GH_Vector)ComponentTestHelper.GetOutput(comp, 2);
+
+      Assert.Equal(0, vectorX.Value.X);
+      Assert.Equal(0, vectorX.Value.Y);
+      Assert.Equal(1, vectorX.Value.Z);
+
+      Assert.Equal(0, vectorY.Value.X);
+      Assert.Equal(1, vectorY.Value.Y);
+      Assert.Equal(0, vectorY.Value.Z);
+
+      Assert.Equal(-1, vectorZ.Value.X);
+      Assert.Equal(0, vectorZ.Value.Y);
+      Assert.Equal(0, vectorZ.Value.Z);
+
+      ComponentTestHelper.DrawViewportMeshesAndWiresTest(comp);
+    }
+
+    [Fact]
+    public void ElementLocalAxesTest() {
+      var comp = new LocalAxes();
+      comp.CreateAttributes();
+      var element = new GsaElement1d(new LineCurve(new Point3d(0, 0, 0), new Point3d(0, 0, 10)));
+      var goo = new GsaElement1dGoo(element);
+      ComponentTestHelper.SetInput(comp, goo);
+
+      var vectorX = (GH_Vector)ComponentTestHelper.GetOutput(comp, 0);
+      var vectorY = (GH_Vector)ComponentTestHelper.GetOutput(comp, 1);
+      var vectorZ = (GH_Vector)ComponentTestHelper.GetOutput(comp, 2);
+
+      Assert.Equal(0, vectorX.Value.X);
+      Assert.Equal(0, vectorX.Value.Y);
+      Assert.Equal(1, vectorX.Value.Z);
+
+      Assert.Equal(0, vectorY.Value.X);
+      Assert.Equal(1, vectorY.Value.Y);
+      Assert.Equal(0, vectorY.Value.Z);
+
+      Assert.Equal(-1, vectorZ.Value.X);
+      Assert.Equal(0, vectorZ.Value.Y);
+      Assert.Equal(0, vectorZ.Value.Z);
+
+      ComponentTestHelper.DrawViewportMeshesAndWiresTest(comp);
+    }
+  }
+}
