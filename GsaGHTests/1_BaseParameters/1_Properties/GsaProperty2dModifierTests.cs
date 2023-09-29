4a1bc0a7ca80acd302775ff2b7b3ba2398aabd15
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-09-29 11:14:34 +0200

    GsaProperty2dModifier ToString Test

--- GsaGHTests/1_BaseParameters/1_Properties/GsaProperty2dModifierTests.cs
+++ GsaGHTests/1_BaseParameters/1_Properties/GsaProperty2dModifierTests.cs
@@ -25,6 +25,9 @@ namespace GsaGHTests.Parameters {
       Assert.Equal(0.1, modifier.Shear.As(RatioUnit.DecimalFraction));
       Assert.Equal(0.1, modifier.Volume.As(RatioUnit.DecimalFraction));
       Assert.Equal(0.1, modifier.AdditionalMass.As(AreaDensityUnit.KilogramPerSquareMeter));
+      Assert.Equal(
+        "In-plane:10% Bending:10% Shear:10% Volume:10% Add.Mass:0.1kg/m²", 
+        modifier.ToString());
     }
 
     [Fact]
@@ -43,6 +46,9 @@ namespace GsaGHTests.Parameters {
       Assert.Equal(0.1, modifier.Shear.As(LengthUnit.Meter));
       Assert.Equal(0.1, modifier.Volume.As(LengthUnit.Meter));
       Assert.Equal(0.1, modifier.AdditionalMass.As(AreaDensityUnit.KilogramPerSquareMeter));
+      Assert.Equal(
+        "In-plane:0.1m²/m Bending:0.1m⁴/m Shear:0.1m²/m Volume:0.1m³/m² Add.Mass:0.1kg/m²", 
+        modifier.ToString());
     }
 
     [Fact]
