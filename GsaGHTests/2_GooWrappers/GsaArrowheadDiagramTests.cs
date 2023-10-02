1aeb8dfe34a4d20394ded8b524eabb3536f004fc
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-10-02 16:51:02 +0200

    GsaArrowheadDiagramTests

---
+++ GsaGHTests/2_GooWrappers/GsaArrowheadDiagramTests.cs
@@ -0,0 +1,47 @@
+﻿using System.Drawing;
+using System.Linq;
+using GsaAPI;
+using GsaGH.Parameters;
+using GsaGHTests.Helper;
+using Xunit;
+using DiagramType = GsaGH.Parameters.DiagramType;
+
+namespace GsaGHTests.Parameters {
+  [Collection("GrasshopperFixture collection")]
+  public class GsaArrowheadDiagramTests {
+    public static GraphicDrawResult Element1dUniformLoadDiagramResults() {
+      var graphic = new DiagramSpecification() {
+        ListDefinition = "All",
+        Type = GsaAPI.DiagramType.Load1dPatchForce,
+        Cases = "A1",
+        ScaleFactor = 1,
+        IsNormalised = true,
+        StructuralScale = 1,
+      };
+
+      var model = new GsaAPI.Model(GsaFile.SteelDesignSimple);
+      return model.GetDiagrams(graphic);
+    }
+
+    [Fact]
+    public void ConstructorTest() {
+      GraphicDrawResult graphic = Element1dUniformLoadDiagramResults();
+      var arrowhead = new GsaArrowheadDiagram(graphic.Triangles, 1, Color.Empty);
+
+      Assert.Equal(DiagramType.ArrowHead, arrowhead.DiagramType);
+      Assert.Equal("Arrowhead Diagram", arrowhead.TypeName);
+      Assert.Equal("A GSA arrowhead diagram.", arrowhead.TypeDescription);
+      Assert.Equal(graphic.Triangles.FirstOrDefault().Colour, arrowhead.Value.VertexColors.FirstOrDefault());
+    }
+
+    [Fact]
+    public void CustomColorTest() {
+      GraphicDrawResult graphic = Element1dUniformLoadDiagramResults();
+      var arrowhead = new GsaArrowheadDiagram(graphic.Triangles, 1, Color.Teal);
+
+      Assert.Equal(Color.Teal.ToArgb(), arrowhead.Value.VertexColors.FirstOrDefault().ToArgb());
+    }
+
+
+  }
+}
---
+++ GsaGHTests/2_GooWrappers/GsaDiagramGooTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Drawing;
+using System.Linq;
+using Grasshopper.Kernel.Types;
+using GsaAPI;
+using GsaGH.Parameters;
+using GsaGHTests.GooWrappers;
+using GsaGHTests.Helper;
+using Rhino.Geometry;
+using Xunit;
+using DiagramType = GsaGH.Parameters.DiagramType;
+
+namespace GsaGHTests.Parameters {
+  [Collection("GrasshopperFixture collection")]
+  public class GsaDiagramGooTests {
+
+    [Fact]
+    public void ArrowheadCastToTest() {
+      GraphicDrawResult graphic = 
+        GsaArrowheadDiagramTests.Element1dUniformLoadDiagramResults();
+      var goo = new GsaDiagramGoo(new GsaArrowheadDiagram(graphic.Triangles, 1, Color.Empty));
+
+      GH_Mesh m = null;
+      Assert.True(goo.CastTo(ref m));
+      Assert.NotNull(m);
+
+      double n = 0;
+      Assert.False(goo.CastTo(ref n));
+    }
+
+    [Fact]
+    public void DrawViewportMeshesAndWiresArrowheadTest() {
+      GraphicDrawResult graphic =
+        GsaArrowheadDiagramTests.Element1dUniformLoadDiagramResults();
+      var goo = new GsaDiagramGoo(new GsaArrowheadDiagram(graphic.Triangles, 1, Color.Empty));
+      GH_OasysGeometryGooTests.DrawViewportMeshesAndWiresTest(goo);
+    }
+  }
+}
