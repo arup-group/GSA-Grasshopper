f9d42630ac3fdebd9ca2fb4db579860fb9cc6d54
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-10-03 18:08:29 +0200

    ReactionForcesTests

---
+++ GsaGHTests/3_Components/5_Results/ReactionForcesTests.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+using Grasshopper.Kernel;
+using GsaGH.Components;
+using GsaGH.Parameters;
+using GsaGHTests.Helpers;
+using GsaGHTests.Model;
+using OasysGH.Components;
+using OasysGH.Parameters;
+using Xunit;
+
+namespace GsaGHTests.Results {
+  [Collection("GrasshopperFixture collection")]
+  public class ReactionForcesTests {
+    [Fact]
+    public void TestAnalysisNoInputs() {
+      var comp = new ReactionForces();
+      var result = (GsaResultGoo)ComponentTestHelper.GetOutput(
+        GetResultsTest.ResultsComponentMother());
+      ComponentTestHelper.SetInput(comp, result);
+      var output = (GH_UnitNumber)ComponentTestHelper.GetOutput(comp, 2);
+      Assert.Equal(37.50, output.Value.As(OasysUnits.Units.ForceUnit.Kilonewton));
+    }
+  }
+}
