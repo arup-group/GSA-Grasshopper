64be95543a448673f2b8e0008ae96b8ee023f8b3
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-10-03 16:43:54 +0200

    move enum to own file

---
+++ GsaGH/Parameters/Enums/AnalysisTaskType.cs
@@ -0,0 +1,22 @@
+﻿namespace GsaGH.Parameters {
+  public enum AnalysisTaskType {
+    Static = 1,
+    Buckling = 3,
+    StaticPDelta = 4,
+    NonlinearStatic = 8,
+    ModalDynamic = 2,
+    ModalPDelta = 5,
+    Ritz = 32,
+    RitzPDelta = 33,
+    ResponseSpectrum = 6,
+    PseudoResponseSpectrum = 42,
+    LinearTimeHistory = 15,
+    Harmonic = 14,
+    Footfall = 34,
+    Periodic = 35,
+    FormFinding = 9,
+    Envelope = 37,
+    ModelStability = 39,
+    ModelStabilityPDelta = 40,
+  }
+}
