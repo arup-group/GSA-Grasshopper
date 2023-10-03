8214dedcb518154a345e81fadde98927e86948b4
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-10-03 16:45:40 +0200

    analysis components tests

---
+++ GsaGHTests/3_Components/4_Analysis/CreateAnalysisTaskTests.cs
@@ -0,0 +1,36 @@
+﻿using GsaGH.Components;
+using GsaGH.Parameters;
+using GsaGHTests.Helpers;
+using OasysGH.Components;
+using Xunit;
+
+namespace GsaGHTests.Components.Analysis {
+  [Collection("GrasshopperFixture collection")]
+  public class CreateAnalysisTaskTests {
+
+    public static GH_OasysComponent ComponentMother() {
+      var comp = new CreateAnalysisTask();
+      comp.CreateAttributes();
+
+      ComponentTestHelper.SetInput(comp, "my Task", 0);
+      var output = (GsaAnalysisCaseGoo)ComponentTestHelper.GetOutput(
+        CreateAnalysisCaseTests.ComponentMother());
+      ComponentTestHelper.SetInput(comp, output, 1);
+
+      return comp;
+    }
+
+    [Fact]
+    public void CreateComponentTest() {
+      GH_OasysComponent comp = ComponentMother();
+
+      var output = (GsaAnalysisTaskGoo)ComponentTestHelper.GetOutput(comp);
+
+      Assert.Equal("my Task", output.Value.Name);
+      Assert.Equal(AnalysisTaskType.Static, output.Value.Type);
+      Assert.Equal("1.35A1 + 1.5A2", output.Value.Cases[0].Name);
+      Assert.Equal("my Case", output.Value.Cases[0].Name);
+      Assert.Equal("1.4L1 + 0.8L3", output.Value.Cases[0].Definition);
+    }
+  }
+}
