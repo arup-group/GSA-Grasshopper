734872ddd29c4085d74071238ba6ff5d478ef0ea
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-10-20 12:40:52 +0200

    fix pythagorean helper for baseunits not in API unit (angle unit)

--- GsaGH/Parameters/5_Results/Quantities/ResultUtility.cs
+++ GsaGH/Parameters/5_Results/Quantities/ResultUtility.cs
@@ -7,9 +7,9 @@ using System.Linq;
 namespace GsaGH.Parameters.Results {
   internal static class ResultUtility {
     internal static T PythagoreanQuadruple<T>(T x, T y, T z, Enum unit) where T : IQuantity {
-      double a = x.As(x.QuantityInfo.BaseUnitInfo.Value);
-      double b = y.As(y.QuantityInfo.BaseUnitInfo.Value);
-      double c = z.As(z.QuantityInfo.BaseUnitInfo.Value);
+      double a = x.As(unit);
+      double b = y.As(unit);
+      double c = z.As(unit);
       double pyth = Math.Sqrt((a * a) + (b * b) + (c * c));
       return (T)Quantity.From(pyth, unit);
     }
