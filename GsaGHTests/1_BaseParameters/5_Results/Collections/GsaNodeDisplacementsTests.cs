5571b15c5f3c960b149d987f8f6f02fddee11ff4
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-10-20 13:14:08 +0200

    NodeDisplacementTests

---
+++ GsaGHTests/1_BaseParameters/5_Results/Collections/GsaNodeDisplacementsTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using GsaAPI;
+using GsaGH.Components;
+using GsaGH.Helpers.GsaApi;
+using GsaGH.Parameters;
+using GsaGH.Parameters.Results;
+using GsaGHTests.Helper;
+using OasysUnits;
+using Xunit;
+using LengthUnit = OasysUnits.Units.LengthUnit;
+
+namespace GsaGHTests.Parameters.Results {
+  [Collection("GrasshopperFixture collection")]
+  public partial class GsaNodeDisplacementsTests {
+    [Fact]
+    public void GsaNodeDisplacementsFromAnalysisCaseTest() {
+      // Assemble
+      var result = (GsaResult2)GsaResult2Tests.AnalysisCaseResult(GsaFile.SteelDesignComplex, 1);
+
+      // Act
+      string nodeList = "442 to 468";
+      GsaNodeDisplacements resultSet = result.NodeDisplacementValues(nodeList);
+
+      // Assert node IDs
+      var expectedIds = result.Model.Model.Nodes(nodeList).Keys.ToList();
+      Assert.Equal(expectedIds, resultSet.Ids);
+
+      // Assert result values
+      List<double> expectedX = ExpectedDisplacementXInMillimeter();
+      int i = 0;
+      foreach (int id in resultSet.Ids) {
+        Collection<IDisplacement> displacementQuantity = resultSet.Results[id];
+        // for analysis case results we expect only one value in the collection
+        Assert.Single(displacementQuantity);
+        double x = ResultHelper.RoundToSignificantDigits(
+          displacementQuantity[0].X.Millimeters, 4);
+        Assert.Equal(expectedX[i++], x);
+      }
+    }
+
+    // these are regression tests, the values are taken directly from GSA results
+    // "Steel_Design_Complex.gwb"
+    private static List<double> ExpectedDisplacementXInMillimeter() {
+      return new List<double>() {
+        1.108,
+0.9107,
+0.6290,
+0.2078,
+0.1886,
+0.5299,
+1.140,
+1.923,
+2.786,
+3.646,
+4.429,
+5.087,
+5.601,
+5.973,
+6.214,
+6.349,
+6.409,
+6.426,
+6.423,
+6.419,
+6.417,
+0.3396,
+0.2390,
+0.01895,
+-0.1426,
+0.01656,
+0.4770
+      };
+    }
+  }
+}
---
+++ GsaGHTests/1_BaseParameters/5_Results/Collections/GsaResult2Tests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using GsaAPI;
+using GsaGH.Components;
+using GsaGH.Helpers.GsaApi;
+using GsaGH.Parameters;
+using GsaGH.Parameters.Results;
+using GsaGHTests.Helper;
+using OasysUnits;
+using Xunit;
+using LengthUnit = OasysUnits.Units.LengthUnit;
+
+namespace GsaGHTests.Parameters.Results {
+  [Collection("GrasshopperFixture collection")]
+  public partial class GsaResult2Tests {
+    public static IGsaResult AnalysisCaseResult(string file, int caseId) {
+      var apiModel = new GsaAPI.Model(file);
+      var model = new GsaModel(apiModel);
+      ReadOnlyDictionary<int, AnalysisCaseResult> analysisCaseResults = model.Model.Results();
+      return new GsaResult2(model, analysisCaseResults[caseId], caseId);
+    }
+
+    public static IGsaResult CombinationCaseResult(string file, int caseId, IEnumerable<int> permutations = null) {
+      var apiModel = new GsaAPI.Model(file);
+      var model = new GsaModel(apiModel);
+      ReadOnlyDictionary<int, CombinationCaseResult> combinationCaseResults = model.Model.CombinationCaseResults();
+      if (permutations == null) {
+        permutations = new List<int>() { 1 };
+      }
+
+      return new GsaResult2(model, combinationCaseResults[caseId], caseId, permutations);
+    }
+  }
+}
