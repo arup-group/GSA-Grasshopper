0ddf403e4f5b5d824127924efa86e3044465d51c
Tilman Reinhardt <tilman.reinhardt@arup.com>
2023-10-27 12:25:30 +0200

    GSAGH-366 refactored extrema

--- GsaGH/Parameters/5_Results/4_Subsets/INodeResultSubset.cs
+++ GsaGH/Parameters/5_Results/4_Subsets/INodeResultSubset.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace GsaGH.Parameters.Results {
-  public interface INodeResultSubset<T1, T2> where T1 : IResultItem where T2 : IResultExtrema {
+  public interface INodeResultSubset<T1, T2> where T1 : IResultItem {
     T2 Max { get; }
     T2 Min { get; }
     IList<int> Ids { get; }
@@ -12,7 +12,7 @@ namespace GsaGH.Parameters.Results {
     /// </para>
     /// Value = Collection of results, one for each permutation. Collection will have 1 item in case of AnalysisCase
     /// </summary>
-    ConcurrentDictionary<int, Collection<T1>> Subset { get; }
+    IDictionary<int, Collection<T1>> Subset { get; }
     T1 GetExtrema(NodeExtremaKey key);
   }
 }
--- GsaGH/Parameters/5_Results/8_ResultInterfaces/IResultVector6.cs
+++ GsaGH/Parameters/5_Results/8_ResultInterfaces/IResultVector6.cs
@@ -1,5 +1,4 @@
 ﻿namespace GsaGH.Parameters.Results {
-  public interface IResultVector6<T1, T2> 
-    : IResultVector3InAxis<T1>, IResultVector3AboutAxis<T2> {
+  public interface IResultVector6<T1, T2> : IResultVector3InAxis<T1>, IResultVector3AboutAxis<T2> {
   }
 }
