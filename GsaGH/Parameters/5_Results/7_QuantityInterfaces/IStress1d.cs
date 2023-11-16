1625bf7b6117e4fb39a92b02c79be9827e41e163
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-11-16 15:43:19 +0100

    stress1d

---
+++ GsaGH/Parameters/5_Results/7_QuantityInterfaces/IStress1d.cs
@@ -0,0 +1,15 @@
+﻿using OasysUnits;
+
+namespace GsaGH.Parameters.Results {
+  public interface IStress1d : IResultItem {
+    Pressure Axial { get; }
+    Pressure BendingYyNegativeZ { get; }
+    Pressure BendingYyPositiveZ { get; }
+    Pressure BendingZzNegativeY { get; }
+    Pressure BendingZzPositiveY { get; }
+    Pressure CombinedC1 { get; }
+    Pressure CombinedC2 { get; }
+    Pressure ShearY { get; }
+    Pressure ShearZ { get; }
+  }
+}
