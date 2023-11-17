f1901f8bf105b4becf0ba27aac842c2debd18199
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-11-17 11:03:02 +0100

    stress regression tests

---
+++ GsaGHTests/1_BaseParameters/5_Results/Collections/RegressionValues/Element1d/Element1dStressC4p1.cs
@@ -0,0 +1,124 @@
+﻿using System.Collections.Generic;
+
+namespace GsaGHTests.Parameters.Results {
+  public class Element1dStressC4p1 {
+    // these are regression tests, the values are taken directly from GSA results
+    // "Steel_Design_Complex.gwb" for elements 2 to 3 for combination case C4p1
+    internal static List<double> AxialInMPa() {
+      return new List<double>() {
+        -27.91,
+-27.91,
+-27.91,
+-27.91,
+-27.91,
+-27.91,
+-27.91,
+-27.91
+      };
+    }
+
+    internal static List<double> SyInMPa() {
+      return new List<double>() {
+        8.153,
+8.153,
+8.153,
+8.153,
+-8.153,
+-8.153,
+-8.153,
+-8.153
+      };
+    }
+
+    internal static List<double> SzInMPa() {
+      return new List<double>() {
+        27.02,
+27.02,
+27.02,
+27.02,
+27.02,
+27.02,
+27.02,
+27.02
+      };
+    }
+
+    internal static List<double> ByPosInMPa() {
+      return new List<double>() {
+        -119.8,
+-36.36,
+47.09,
+130.6,
+-119.8,
+-36.36,
+47.09,
+130.6
+      };
+    }
+
+    internal static List<double> ByNegInMPa() {
+      return new List<double>() {
+        119.8,
+36.36,
+-47.09,
+-130.5,
+119.8,
+36.36,
+-47.09,
+-130.5
+      };
+    }
+
+    internal static List<double> BzPosInMPa() {
+      return new List<double>() {
+        -37.22,
+-12.04,
+13.14,
+38.32,
+37.22,
+12.04,
+-13.14,
+-38.32
+      };
+    }
+
+    internal static List<double> BzNegInMPa() {
+      return new List<double>() {
+        37.22,
+12.04,
+-13.14,
+-38.32,
+-37.22,
+-12.04,
+13.14,
+38.32
+      };
+    }
+
+    internal static List<double> C1InMPa() {
+      return new List<double>() {
+        125.7,
+19.39,
+31.11,
+137.4,
+125.7,
+19.39,
+31.11,
+137.4
+      };
+    }
+
+    internal static List<double> C2InMPa() {
+      return new List<double>() {
+        -181.5,
+-75.21,
+-86.92,
+-193.2,
+-181.6,
+-75.22,
+-86.92,
+-193.2
+      };
+    }
+  }
+}
--- GsaGHTests/1_BaseParameters/5_Results/Collections/TestsResultHelper.cs.cs
+++ GsaGHTests/1_BaseParameters/5_Results/Collections/TestsResultHelper.cs.cs
@@ -1,5 +1,6 @@
 ﻿using GsaGH.Helpers.GsaApi;
 using GsaGH.Parameters.Results;
+using static GsaGHTests.Parameters.Results.Element1dStressTests;
 
 namespace GsaGHTests.Parameters.Results {
   public static class TestsResultHelper {
@@ -165,6 +166,49 @@ namespace GsaGHTests.Parameters.Results {
       return ResultHelper.RoundToSignificantDigits(d, 4);
     }
 
+    public static double ResultsHelper(IStress1d result, ResultStress1dHelperEnum component) {
+      double d = 0;
+      switch (component) {
+        case ResultStress1dHelperEnum.Axial:
+          d = result.Axial.Megapascals;
+          break;
+
+        case ResultStress1dHelperEnum.ShearY:
+          d = result.ShearY.Megapascals;
+          break;
+
+        case ResultStress1dHelperEnum.ShearZ:
+          d = result.ShearZ.Megapascals;
+          break;
+
+        case ResultStress1dHelperEnum.ByPos:
+          d = result.BendingYyPositiveZ.Megapascals;
+          break;
+
+        case ResultStress1dHelperEnum.ByNeg:
+          d = result.BendingYyNegativeZ.Megapascals;
+          break;
+
+        case ResultStress1dHelperEnum.BzPos:
+          d = result.BendingZzPositiveY.Megapascals;
+          break;
+
+        case ResultStress1dHelperEnum.BzNeg:
+          d = result.BendingZzNegativeY.Megapascals;
+          break;
+
+        case ResultStress1dHelperEnum.C1:
+          d = result.CombinedC1.Megapascals;
+          break;
+
+        case ResultStress1dHelperEnum.C2:
+          d = result.CombinedC2.Megapascals;
+          break;
+      }
+
+      return ResultHelper.RoundToSignificantDigits(d, 4);
+    }
+
     public static double ResultsHelper(
       IElement1dResultSubset<IElement1dDisplacement, IDisplacement, ResultVector6<Element1dExtremaKey>> result,
       ResultVector6HelperEnum component, bool max) {
@@ -243,6 +287,53 @@ namespace GsaGHTests.Parameters.Results {
 
         case ResultVector6HelperEnum.Xxyyzz:
           d = result.GetExtrema(extrema.Xxyyzz).Xxyyzz.KilonewtonMeters;
+          break;
+      }
+
+      return ResultHelper.RoundToSignificantDigits(d, 4);
+    }
+
+
+    public static double ResultsHelper(
+      IElement1dResultSubset<IElement1dStress, IStress1d, ResultStress1d<Element1dExtremaKey>> result,
+      Element1dStressTests.ResultStress1dHelperEnum component, bool max) {
+      double d = 0;
+      ResultStress1d<Element1dExtremaKey> extrema = max ? result.Max : result.Min;
+      switch (component) {
+        case Element1dStressTests.ResultStress1dHelperEnum.Axial:
+          d = result.GetExtrema(extrema.Axial).Axial.Megapascals;
+          break;
+
+        case Element1dStressTests.ResultStress1dHelperEnum.ShearY:
+          d = result.GetExtrema(extrema.ShearY).ShearY.Megapascals;
+          break;
+
+        case Element1dStressTests.ResultStress1dHelperEnum.ShearZ:
+          d = result.GetExtrema(extrema.ShearZ).ShearZ.Megapascals;
+          break;
+
+        case Element1dStressTests.ResultStress1dHelperEnum.ByPos:
+          d = result.GetExtrema(extrema.BendingYyPositiveZ).BendingYyPositiveZ.Megapascals;
+          break;
+
+        case Element1dStressTests.ResultStress1dHelperEnum.ByNeg:
+          d = result.GetExtrema(extrema.BendingYyNegativeZ).BendingYyNegativeZ.Megapascals;
+          break;
+
+        case Element1dStressTests.ResultStress1dHelperEnum.BzPos:
+          d = result.GetExtrema(extrema.BendingZzPositiveY).BendingZzPositiveY.Megapascals;
+          break;
+
+        case Element1dStressTests.ResultStress1dHelperEnum.BzNeg:
+          d = result.GetExtrema(extrema.BendingZzNegativeY).BendingZzNegativeY.Megapascals;
+          break;
+
+        case Element1dStressTests.ResultStress1dHelperEnum.C1:
+          d = result.GetExtrema(extrema.CombinedC1).CombinedC1.Megapascals;
+          break;
+
+        case Element1dStressTests.ResultStress1dHelperEnum.C2:
+          d = result.GetExtrema(extrema.CombinedC2).CombinedC2.Megapascals;
           break;
       }
 
