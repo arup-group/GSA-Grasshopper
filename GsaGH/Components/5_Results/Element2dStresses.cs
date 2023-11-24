a59a819c191d06060f1cd1a5d6dd250bfb95db6d
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-11-24 13:15:25 +0100

    2d stress result implementation

--- GsaGH/Components/5_Results/Element2dStresses.cs
+++ GsaGH/Components/5_Results/Element2dStresses.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Drawing;
 using System.Linq;
 using System.Threading.Tasks;
@@ -8,10 +9,12 @@ using Grasshopper;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Data;
 using Grasshopper.Kernel.Types;
+using GsaAPI;
 using GsaGH.Components.Helpers;
 using GsaGH.Helpers;
 using GsaGH.Helpers.GH;
 using GsaGH.Parameters;
+using GsaGH.Parameters.Results;
 using GsaGH.Properties;
 using OasysGH;
 using OasysGH.Components;
@@ -39,7 +42,7 @@ namespace GsaGH.Components {
 
     public override void SetSelected(int i, int j) {
       _selectedItems[i] = _dropDownItems[i][j];
-      _stresshUnit = (PressureUnit)UnitsHelper.Parse(typeof(PressureUnit), _selectedItems[i]);
+      _stresshUnit = (PressureUnit)UnitsHelper.Parse(typeof(PressureUnit), _selectedItems[1]);
       base.UpdateUI();
     }
 
@@ -56,11 +59,15 @@ namespace GsaGH.Components {
 
     protected override void InitialiseDropdowns() {
       _spacerDescriptions = new List<string>(new[] {
+        "Envelope",
         "Unit",
       });
 
       _dropDownItems = new List<List<string>>();
       _selectedItems = new List<string>();
+
+      _dropDownItems.Add(ExtremaHelper.Tensor3Stresses.ToList());
+      _selectedItems.Add(_dropDownItems[0][0]);
 
       _dropDownItems.Add(UnitsHelper.GetFilteredAbbreviations(EngineeringUnits.Stress));
       _selectedItems.Add(Pressure.GetAbbreviation(_stresshUnit));
@@ -73,9 +80,9 @@ namespace GsaGH.Components {
         GH_ParamAccess.list);
       pManager.AddParameter(new GsaElementMemberListParameter());
       pManager[1].Optional = true;
-      pManager.AddNumberParameter("Stress Layer", "σL",
+      pManager.AddIntegerParameter("Stress Layer", "σL",
         "Layer within the cross-section to get results." + Environment.NewLine
-        + "Input a number between -1 and 1, representing the normalised thickness,"
+        + "Input an integer between -1 and 1, representing the normalised thickness,"
         + Environment.NewLine + "default value is zero => middle of the element.",
         GH_ParamAccess.item, 0);
     }
@@ -100,13 +107,19 @@ namespace GsaGH.Components {
     }
 
     protected override void SolveInternal(IGH_DataAccess da) {
-      var result = new GsaResult();
+      GsaResult2 result = null;
       string elementlist = "All";
 
-      double layer = 0;
-      var ghType1 = new GH_String();
-      if (da.GetData(2, ref ghType1)) {
-        GH_Convert.ToDouble(ghType1, out layer, GH_Conversion.Both);
+      Layer2d layer = Layer2d.Middle;
+      var ghLayer = new GH_Number();
+      if (da.GetData(2, ref ghLayer)) {
+        GH_Convert.ToInt32(ghLayer, out int val, GH_Conversion.Both);
+        layer = val switch {
+          -1 => Layer2d.Bottom,
+          0 => Layer2d.Middle,
+          1 => Layer2d.Top,
+          _ => throw new ArgumentException("Layer must be between -1 and 1"),
+        };
       }
 
       var outXx = new DataTree<GH_UnitNumber>();
@@ -123,87 +136,63 @@ namespace GsaGH.Components {
 
       foreach (GH_ObjectWrapper ghTyp in ghTypes) {
         switch (ghTyp?.Value) {
+          case GsaResultGoo goo:
+            result = new GsaResult2((GsaResult)goo.Value);
+            elementlist = Inputs.GetElementListDefinition(this, da, 1, result.Model);
+            break;
+
           case null:
             this.AddRuntimeWarning("Input is null");
             return;
-
-          case GsaResultGoo goo:
-            result = (GsaResult)goo.Value;
-            elementlist = Inputs.GetElementListDefinition(this, da, 1, result.Model);
-            break;
 
           default:
             this.AddRuntimeError("Error converting input to GSA Result");
             return;
         }
 
-        List<GsaResultsValues> vals
-          = result.Element2DStressValues(elementlist, layer, _stresshUnit);
+        ReadOnlyCollection<int> elementIds = result.ElementIds(elementlist);
+        IEntity2dResultSubset<IEntity2dQuantity<IStress>, IStress, ResultTensor3<Entity2dExtremaKey>> resultSet =
+          result.Element2dStresses.ResultSubset(elementIds, layer);
 
         List<int> permutations = result.SelectedPermutationIds ?? new List<int>() {
           1,
         };
         if (permutations.Count == 1 && permutations[0] == -1) {
-          permutations = Enumerable.Range(1, vals.Count).ToList();
+          permutations = Enumerable.Range(1, resultSet.Subset.Values.First().Count).ToList();
         }
 
-        foreach (int perm in permutations) {
-          if (vals[perm - 1].XyzResults.Count == 0 & vals[perm - 1].XxyyzzResults.Count == 0) {
-            string acase = result.ToString().Replace('}', ' ').Replace('{', ' ');
-            this.AddRuntimeWarning("Case " + acase + " contains no Element2D results.");
-            continue;
-          }
-
-          Parallel.For(0, 2, thread => // split computation in two for xyz and xxyyzz
-          {
-            switch (thread) {
-              case 0: {
-                foreach (KeyValuePair<int, ConcurrentDictionary<int, GsaResultQuantity>> kvp in
-                  vals[perm - 1].XyzResults) {
-                  int elementId = kvp.Key;
-                  ConcurrentDictionary<int, GsaResultQuantity> res = kvp.Value;
-                  if (res.Count == 0) {
-                    continue;
-                  }
-
-                  var path = new GH_Path(result.CaseId,
-                    result.SelectedPermutationIds == null ? 0 : perm, elementId);
-
-                  outXx.AddRange(res.Select(x => new GH_UnitNumber(x.Value.X.ToUnit(_stresshUnit))),
-                    path); // use ToUnit to capture changes in dropdown
-                  outYy.AddRange(res.Select(x => new GH_UnitNumber(x.Value.Y.ToUnit(_stresshUnit))),
-                    path);
-                  outZz.AddRange(res.Select(x => new GH_UnitNumber(x.Value.Z.ToUnit(_stresshUnit))),
-                    path);
-                }
-
-                break;
-              }
-              case 1: {
-                foreach (KeyValuePair<int, ConcurrentDictionary<int, GsaResultQuantity>> kvp in
-                  vals[perm - 1].XxyyzzResults) {
-                  int elementId = kvp.Key;
-                  ConcurrentDictionary<int, GsaResultQuantity> res = kvp.Value;
-                  if (res.Count == 0) {
-                    continue;
-                  }
-
-                  var path = new GH_Path(result.CaseId,
-                    result.SelectedPermutationIds == null ? 0 : perm, elementId);
-
-                  outXy.AddRange(res.Select(x => new GH_UnitNumber(x.Value.X.ToUnit(_stresshUnit))),
-                    path); // always use [rad] units
-                  outYz.AddRange(res.Select(x => new GH_UnitNumber(x.Value.Y.ToUnit(_stresshUnit))),
-                    path);
-                  outZx.AddRange(res.Select(x => new GH_UnitNumber(x.Value.Z.ToUnit(_stresshUnit))),
-                    path);
-                }
-
-                break;
-              }
+        if (_selectedItems[0] == ExtremaHelper.Vector6Displacements[0]) {
+          foreach (KeyValuePair<int, Collection<IEntity2dQuantity<IStress>>> kvp in resultSet.Subset) {
+            foreach (int p in permutations) {
+              var path = new GH_Path(result.CaseId, result.SelectedPermutationIds == null ? 0 : p, kvp.Key);
+              outXx.AddRange(kvp.Value[p - 1].Results().Select(
+                r => new GH_UnitNumber(r.Xx.ToUnit(_stresshUnit))), path);
+              outYy.AddRange(kvp.Value[p - 1].Results().Select(
+                r => new GH_UnitNumber(r.Yy.ToUnit(_stresshUnit))), path);
+              outZz.AddRange(kvp.Value[p - 1].Results().Select(
+                r => new GH_UnitNumber(r.Zz.ToUnit(_stresshUnit))), path);
+              outXy.AddRange(kvp.Value[p - 1].Results().Select(
+                r => new GH_UnitNumber(r.Xy.ToUnit(_stresshUnit))), path);
+              outYz.AddRange(kvp.Value[p - 1].Results().Select(
+                r => new GH_UnitNumber(r.Yz.ToUnit(_stresshUnit))), path);
+              outZx.AddRange(kvp.Value[p - 1].Results().Select(
+                r => new GH_UnitNumber(r.Zx.ToUnit(_stresshUnit))), path);
             }
-          });
+          }
+        } else {
+          Entity2dExtremaKey key = ExtremaHelper.StressExtremaKey(resultSet, _selectedItems[0]);
+          IStress extrema = resultSet.GetExtrema(key);
+          int perm = result.CaseType == CaseType.AnalysisCase ? 0 : 1;
+          var path = new GH_Path(result.CaseId, key.Permutation + perm, key.Id);
+          outXx.Add(new GH_UnitNumber(extrema.Xx.ToUnit(_stresshUnit)), path);
+          outYy.Add(new GH_UnitNumber(extrema.Yy.ToUnit(_stresshUnit)), path);
+          outZz.Add(new GH_UnitNumber(extrema.Zz.ToUnit(_stresshUnit)), path);
+          outXy.Add(new GH_UnitNumber(extrema.Xy.ToUnit(_stresshUnit)), path);
+          outYz.Add(new GH_UnitNumber(extrema.Yz.ToUnit(_stresshUnit)), path);
+          outZx.Add(new GH_UnitNumber(extrema.Zx.ToUnit(_stresshUnit)), path);
         }
+
+        PostHog.Result(result.CaseType, 2, GsaResultsValues.ResultType.Stress);
       }
 
       da.SetDataTree(0, outXx);
@@ -212,12 +201,16 @@ namespace GsaGH.Components {
       da.SetDataTree(3, outXy);
       da.SetDataTree(4, outYz);
       da.SetDataTree(5, outZx);
-
-      PostHog.Result(result.CaseType, 2, GsaResultsValues.ResultType.Stress);
     }
 
     protected override void UpdateUIFromSelectedItems() {
-      _stresshUnit = (PressureUnit)UnitsHelper.Parse(typeof(PressureUnit), _selectedItems[0]);
+      if (_selectedItems.Count == 1) {
+        _spacerDescriptions.Insert(0, "Envelope");
+        _dropDownItems.Insert(0, ExtremaHelper.Tensor3Stresses.ToList());
+        _selectedItems.Insert(0, _dropDownItems[0][0]);
+      }
+
+      _stresshUnit = (PressureUnit)UnitsHelper.Parse(typeof(PressureUnit), _selectedItems[1]);
       base.UpdateUIFromSelectedItems();
     }
   }
---
+++ GsaGH/Parameters/5_Results/4_Subsets/Entity2dStressFactory.cs
@@ -0,0 +1,33 @@
+﻿using GsaAPI;
+using System.Collections.ObjectModel;
+
+namespace GsaGH.Parameters.Results {
+  public static partial class Entity2dResultsFactory {
+    public static Collection<IEntity2dQuantity<IStress>> CreateStresses(
+      ReadOnlyCollection<Tensor3> results) {
+      return new Collection<IEntity2dQuantity<IStress>> {
+              CreateFromApiCollection(results)
+             };
+    }
+
+    public static Collection<IEntity2dQuantity<IStress>> CreateStresses(
+      ReadOnlyCollection<ReadOnlyCollection<Tensor3>> results) {
+      var permutations = new Collection<IEntity2dQuantity<IStress>>();
+      foreach (ReadOnlyCollection<Tensor3> permutation in results) {
+        permutations.Add(CreateFromApiCollection(permutation));
+      }
+      return permutations;
+    }
+
+    private static IEntity2dQuantity<IStress> CreateFromApiCollection(
+      ReadOnlyCollection<Tensor3> results) {
+      return results.Count switch {
+        4 => new Entity2dTriStress(results),
+        5 => new Entity2dQuadStress(results),
+        7 => new Entity2dTri6Stress(results),
+        9 => new Entity2dQuad8Stress(results),
+        _ => throw new System.ArgumentException("Unable to create 2d result"),
+      };
+    }
+  }
+}
