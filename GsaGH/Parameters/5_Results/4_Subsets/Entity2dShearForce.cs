adec0035c75ae06dd5f64b478ef61af421aa3ed9
DominikaLos <83698580+DominikaLos@users.noreply.github.com>
2023-11-27 14:19:13 +0100

    Task/gsagh 433 element2d shear force cache (#590)
    * cache classes
    
    * regression tests
    
    * rename: Vx => Qx

---
+++ GsaGH/Parameters/5_Results/4_Subsets/Entity2dShearForce.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using OasysUnits;
+
+namespace GsaGH.Parameters.Results {
+  public class Entity2dShearForce : IEntity2dResultSubset<IEntity2dQuantity<IShear2d>, IShear2d, ResultVector2<Entity2dExtremaKey>> {
+    public ResultVector2<Entity2dExtremaKey> Max { get; private set; }
+    public ResultVector2<Entity2dExtremaKey> Min { get; private set; }
+    public IList<int> Ids { get; private set; }
+
+    public ConcurrentDictionary<int, Collection<IEntity2dQuantity<IShear2d>>> Subset { get; }
+      = new ConcurrentDictionary<int, Collection<IEntity2dQuantity<IShear2d>>>();
+
+    public Entity2dShearForce(ConcurrentDictionary<int, Collection<IEntity2dQuantity<IShear2d>>> results) {
+      Subset = results;
+      Ids = results.Keys.OrderBy(x => x).ToList();
+      (Max, Min) = results.GetResultVector2Entity2dExtremaKeys();
+    }
+
+    public IShear2d GetExtrema(IEntity2dExtremaKey key) {
+      return Subset[key.Id][key.Permutation].Results()[key.VertexId];
+    }
+  }
+}
