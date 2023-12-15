590e2d47c288981920a70970542d459cc0e9a968
Kristjan Nielsen <kristjan.nielsen@arup.com>
2023-12-15 20:54:23 +0100

    update min GSA version to 10.2.4 and copyright year 2024

--- GsaGH/GsaGHInfo.cs
+++ GsaGH/GsaGHInfo.cs
@@ -33,10 +33,10 @@ namespace GsaGH {
     public override string Version => isBeta ? Vers + "-beta" : Vers;
     internal const string Company = "Oasys";
     internal const string Contact = "https://www.oasys-software.com/";
-    internal const string Copyright = "Copyright © Oasys 1985 - 2023";
+    internal const string Copyright = "Copyright © Oasys 1985 - 2024";
     internal const string PluginName = "GsaGH";
     internal const string ProductName = "GSA";
-    internal static int MinGsaVersion = 3;
+    internal static int MinGsaVersion = 4;
     internal const string TermsConditions
       = "Oasys terms and conditions apply. See https://www.oasys-software.com/terms-conditions for details. ";
     internal const string Vers = "1.1.0";
