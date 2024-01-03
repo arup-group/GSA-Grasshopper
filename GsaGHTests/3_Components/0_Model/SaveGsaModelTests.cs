d8684b4c626d3883ecef2a270bb257ef4b23d6e0
DominikaLos <dominika.los@arup.com>
2024-01-03 16:45:35 +0100

    fix

--- GsaGHTests/3_Components/0_Model/SaveGsaModelTests.cs
+++ GsaGHTests/3_Components/0_Model/SaveGsaModelTests.cs
@@ -7,19 +7,23 @@ using Xunit;
 namespace GsaGHTests.Model {
   [Collection("GrasshopperFixture collection")]
   public class SaveGsaModelTests {
-    [Fact]
-    public void SaveGsaModelTest() {
+    [Theory]
+    [InlineData("GSA-Grasshopper_temp2.gwa")]
+    [InlineData("GSA-Grasshopper_temp2.gwb")]
+    public void SaveGsaModelTest(string path) {
       var comp = new SaveGsaModel();
       comp.CreateAttributes();
       ComponentTestHelper.SetInput(comp, ModelTests.GsaModelGooMother);
       ComponentTestHelper.SetInput(comp, true, 1);
-      string tempfilename = Path.GetTempPath() + "GSA-Grasshopper_temp2.gwb";
+      string tempfilename = Path.GetTempPath() + path;
       ComponentTestHelper.SetInput(comp, tempfilename, 2);
       var output = (GsaModelGoo)ComponentTestHelper.GetOutput(comp);
       Assert.NotNull(output);
       Assert.Empty(comp.RuntimeMessages(Grasshopper.Kernel.GH_RuntimeMessageLevel.Warning));
       Assert.Empty(comp.RuntimeMessages(Grasshopper.Kernel.GH_RuntimeMessageLevel.Error));
-      Assert.True(File.Exists(tempfilename));
+      string expectedPath = Path.GetTempPath() + "GSA-Grasshopper_temp2.gwa";
+
+      Assert.True(File.Exists(expectedPath));
     }
   }
 }
