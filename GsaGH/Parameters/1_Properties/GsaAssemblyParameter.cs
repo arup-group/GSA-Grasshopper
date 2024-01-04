e25845da6d0ba64a8583d64311460f097aafe34c
Tilman Reinhardt <tilman.reinhardt@arup.com>
2024-01-04 16:31:35 +0100

    GSAGH-456 started implementing assemblies

---
+++ GsaGH/Parameters/1_Properties/GsaAssemblyParameter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper.Kernel;
+using GsaGH.Helpers.GH;
+using OasysGH.Parameters;
+
+namespace GsaGH.Parameters {
+  /// <summary>
+  /// This class provides a parameter interface for the <see cref="GsaAssemblyGoo" /> type.
+  /// </summary>
+  public class GsaAssemblyParameter : GH_OasysPersistentParam<GsaAssemblyGoo> {
+    public override Guid ComponentGuid => new Guid("4b39f91c-4558-4432-96d1-0bbec98a3447");
+    public override GH_Exposure Exposure => GH_Exposure.secondary;
+    public override string InstanceDescription
+      => m_data.DataCount == 0 ? "Empty " + GsaAssemblyGoo.Name + " parameter" :
+        base.InstanceDescription;
+    public override string TypeName => SourceCount == 0 ? GsaAssemblyGoo.Name : base.TypeName;
+    protected override Bitmap Icon => null;
+
+    public GsaAssemblyParameter() : base(new GH_InstanceDescription(GsaAssemblyGoo.Name,
+      GsaAssemblyGoo.NickName, GsaAssemblyGoo.Description + " parameter", CategoryName.Name(),
+      SubCategoryName.Cat9())) { }
+  }
+}
---
+++ GsaGH/Parameters/2_Geometry/GsaAssembly.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using GsaAPI;
+
+namespace GsaGH.Parameters {
+  /// <summary>
+  /// Assemblies are a way to define an entity that is formed from a collection of elements or members and can be thought of as a super-element. This is not an analysis entity but just a convenience for post-processing such as cut section forces. Typical uses of assemblies include cores, where the core is modelled with 2D finite elements, or trusses where the truss is modelled with top and bottom chords and bracing. In both these cases the assembly is identified by a list of included elements.
+  /// Unlike the analysis elements, an assembly does not have a clearly define orientation and location of reference point so these must be defined explicitly.
+  /// <para>Refer to <see href="https://docs.oasys-software.com/structural/gsa/references/hidr-data-assembly/index.html">Assemblies</see> to read more.</para>
+  /// </summary>
+  public class GsaAssembly {
+    public Guid Guid { get; set; } = Guid.NewGuid();
+    public int Id { get; set; } = 0;
+    public SpringProperty ApiAssembly { get; internal set; }
+
+    public GsaAssembly() {
+      //ApiAssembly = new AssemblyByNumberOfPoints();
+    }
+
+    public GsaAssembly(GsaAssembly other) {
+      Id = other.Id;
+      //ApiAssembly = other.DuplicateApiObject();
+    }
+
+    //internal GsaAssembly(Assembly assembly) {
+    //  ApiAssembly = assembly;
+    //}
+
+    //internal GsaAssembly(KeyValuePair<int, Assembly> assembly) {
+    //  Id = assembly.Key;
+    //  ApiAssembly = assembly.Value;
+    //}
+
+    //public Assembly DuplicateApiObject() {
+
+    //}
+
+    public override string ToString() {
+      return "";
+    }
+  }
+}
