d9c895b8d18c9e6865dd86ca9dd7684fe59e5561
Kristjan Nielsen <kristjan.nielsen@arup.com>
2024-01-08 15:29:15 +0100

    Secion pool duplicate test

--- GsaGHTests/3_Components/4_Analysis/CreateSteelSectionPoolTests.cs
+++ GsaGHTests/3_Components/4_Analysis/CreateSteelSectionPoolTests.cs
@@ -29,5 +29,22 @@ namespace GsaGHTests.Components.Analysis {
       var output = (GsaSectionGoo)ComponentTestHelper.GetOutput(ComponentMother());
       Assert.Equal(7, output.Value.ApiSection.Pool);
     }
+
+    [Fact]
+    public void TestDuplicate() {
+      var comp = new CreateSteelSectionPool();
+      comp.CreateAttributes();
+
+      var section = (GsaSectionGoo)ComponentTestHelper.GetOutput(
+        CreateSectionTests.ComponentMother("STD CH(mm) 40 30 2 1"));
+      Assert.Equal(0, section.Value.ApiSection.Pool);
+
+      ComponentTestHelper.SetInput(comp, 1, 0);
+      ComponentTestHelper.SetInput(comp, section, 1);
+
+      var output = (GsaSectionGoo)ComponentTestHelper.GetOutput(comp);
+      Assert.Equal(0, section.Value.ApiSection.Pool);
+      Assert.Equal(1, output.Value.ApiSection.Pool);
+    }
   }
 }
