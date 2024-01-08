f80bd18d6ae5237cc113c6716f00aa8e97c27aae
Kristjan Nielsen <kristjan.nielsen@arup.com>
2024-01-08 18:27:28 +0100

    reinstate and fix ability to open .gwa files

--- GsaGH/Components/0_Model/OpenModel.cs
+++ GsaGH/Components/0_Model/OpenModel.cs
@@ -3,7 +3,6 @@ using System.Drawing;
 using System.IO;
 using Grasshopper.Kernel;
 using Grasshopper.Kernel.Special;
-using Grasshopper.Kernel.Types;
 using GsaAPI;
 using GsaGH.Helpers.GH;
 using GsaGH.Helpers.GsaApi.EnumMappings;
@@ -37,8 +36,8 @@ namespace GsaGH.Components {
 
     public void OpenFile() {
       var fdi = new OpenFileDialog {
-        Filter = "GSA Files(*.gwb)|*.gwb|All files (*.*)|*.*",
-      }; //"GSA Files(*.gwa; *.gwb)|*.gwa;*.gwb|All files (*.*)|*.*"
+        Filter = "GSA Files (*.gwb)|*.gwb|GSA Text Files (*.gwa)|*.gwa|All files (*.*)|*.*",
+      };
       bool res = fdi.ShowOpenDialog();
 
       if (!res) {
@@ -92,12 +91,8 @@ namespace GsaGH.Components {
 
     protected override void SolveInternal(IGH_DataAccess da) {
       var model = new Model();
-      var ghTyp = new GH_ObjectWrapper();
       string fileName = string.Empty;
       da.GetData(0, ref fileName);
-      if (!fileName.EndsWith(".gwb")) {
-        fileName += ".gwb";
-      }
 
       model.Open(fileName);
 
@@ -112,7 +107,7 @@ namespace GsaGH.Components {
       }
 
       da.SetData(0, new GsaModelGoo(gsaModel));
-      PostHog.ModelIO(GsaGH.PluginInfo.Instance, "openGWB",
+      PostHog.ModelIO(GsaGH.PluginInfo.Instance, $"open{fileName.Substring(fileName.LastIndexOf('.') + 1).ToUpper()}",
         (int)(new FileInfo(fileName).Length / 1024));
     }
 
