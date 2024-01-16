792954b8bccdaacfd244857a127fe72656e56eea
Tilman Reinhardt <tilman.reinhardt@arup.com>
2024-01-16 10:58:53 +0100

    SaveGsaModelTests

--- GsaGHTests/3_Components/0_Model/SaveGsaModelTests.cs
+++ GsaGHTests/3_Components/0_Model/SaveGsaModelTests.cs
@@ -8,21 +8,21 @@ namespace GsaGHTests.Model {
   [Collection("GrasshopperFixture collection")]
   public class SaveGsaModelTests {
     [Theory]
-    [InlineData("GSA-Grasshopper_temp2.gwa")]
-    [InlineData("GSA-Grasshopper_temp2.gwb")]
-    [InlineData("GSA-Grasshopper_temp2.gwc")]
-    public void SaveGsaModelTest(string path) {
+    [InlineData("GSA-Grasshopper1.gwa")]
+    [InlineData("GSA-Grasshopper2.gwb")]
+    [InlineData("GSA-Grasshopper3.gwc")]
+    public void SaveGsaModelTest(string filename) {
       var comp = new SaveGsaModel();
       comp.CreateAttributes();
       ComponentTestHelper.SetInput(comp, ModelTests.GsaModelGooMother);
       ComponentTestHelper.SetInput(comp, true, 1);
-      string tempfilename = Path.GetTempPath() + path;
-      ComponentTestHelper.SetInput(comp, tempfilename, 2);
+      string path = Path.GetTempPath() + filename;
+      ComponentTestHelper.SetInput(comp, path, 2);
       var output = (GsaModelGoo)ComponentTestHelper.GetOutput(comp);
       Assert.NotNull(output);
       Assert.Empty(comp.RuntimeMessages(Grasshopper.Kernel.GH_RuntimeMessageLevel.Warning));
       Assert.Empty(comp.RuntimeMessages(Grasshopper.Kernel.GH_RuntimeMessageLevel.Error));
-      string expectedPath = Path.GetTempPath() + (path.EndsWith("gwc") ? "GSA-Grasshopper_temp2.gwa" : path);
+      string expectedPath = Path.GetTempPath() + (path.EndsWith("gwc") ? "GSA-Grasshopper3.gwa" : filename);
 
       Assert.True(File.Exists(expectedPath));
     }
