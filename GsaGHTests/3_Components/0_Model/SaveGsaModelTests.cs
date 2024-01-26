6ca3e8ebf37900db2af18f5503057eac6ed7005d
Tilman Reinhardt <tilman.reinhardt@arup.com>
2024-01-26 08:09:42 +0100

    save .gwa without results

--- GsaGHTests/3_Components/0_Model/SaveGsaModelTests.cs
+++ GsaGHTests/3_Components/0_Model/SaveGsaModelTests.cs
@@ -1,19 +1,33 @@
 ﻿using System.IO;
 using GsaGH.Components;
 using GsaGH.Parameters;
+using GsaGHTests.Helper;
 using GsaGHTests.Helpers;
+using OasysGH.Components;
 using Xunit;
 
 namespace GsaGHTests.Model {
   [Collection("GrasshopperFixture collection")]
   public class SaveGsaModelTests {
+    public static GsaModelGoo GsaModelGooMother => (GsaModelGoo)ComponentTestHelper.GetOutput(SaveGsaModelMother());
+
+    public static GH_OasysComponent SaveGsaModelMother() {
+      var comp = new OpenModel();
+      comp.CreateAttributes();
+
+      string file = GsaFile.Element2dMultiPropsParentMember;
+      ComponentTestHelper.SetInput(comp, file);
+
+      return comp;
+    }
+
     [Theory]
     [InlineData("GSA-Grasshopper_temp2.gwa")]
     [InlineData("GSA-Grasshopper_temp2.gwb")]
     public void SaveGsaModelTest(string path) {
       var comp = new SaveGsaModel();
       comp.CreateAttributes();
-      ComponentTestHelper.SetInput(comp, ModelTests.GsaModelGooMother);
+      ComponentTestHelper.SetInput(comp, GsaModelGooMother);
       ComponentTestHelper.SetInput(comp, true, 1);
       string tempfilename = Path.GetTempPath() + path;
       ComponentTestHelper.SetInput(comp, tempfilename, 2);
