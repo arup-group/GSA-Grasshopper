270c4b683234b442f3abf78ab907ae350537bbeb
Tilman Reinhardt <tilman.reinhardt@arup.com>
2024-02-09 15:28:41 +0100

    formatting

--- GsaGH/Parameters/3_Loads/GsaLoadCase.cs
+++ GsaGH/Parameters/3_Loads/GsaLoadCase.cs
@@ -7,14 +7,15 @@ namespace GsaGH.Parameters {
   /// When <see cref="IGsaLoad"/>s are applied to the model they are assigned to a load case. Load cases are a convenient way of grouping together a collection of loads that are to be considered acting together, for instance dead loads or live loads. In GSA, the load cases are only used to group loading and are not used directly for analysis.
   /// <para>Refer to <see href="https://docs.oasys-software.com/structural/gsa/references/hidr-data-load-title.html">Load Case Specification</see> to read more.</para>
   /// </summary>
-  public class GsaLoadCase {
+  public class GsaLoadCase
+  {
     public int Id { get; private set; }
     public LoadCase LoadCase;
 
     public GsaLoadCase() { }
 
     public GsaLoadCase(int id) {
-       if (id < 1) {
+      if (id < 1) {
         throw new ArgumentException("LoadCase ID cannot be zero or negative");
       }
 
