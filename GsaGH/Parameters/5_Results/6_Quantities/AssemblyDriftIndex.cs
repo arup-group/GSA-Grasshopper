0deefea7ff3d74fae76de08986af7f7fbc94649d
Tilman Reinhardt <tilman.reinhardt@arup.com>
2024-02-15 16:21:51 +0100

    GSAGH-455 assembly storeys

--- GsaGH/Parameters/5_Results/6_Quantities/AssemblyDriftIndex.cs
+++ GsaGH/Parameters/5_Results/6_Quantities/AssemblyDriftIndex.cs
@@ -5,10 +5,12 @@ using GsaAPI;
 namespace GsaGH.Parameters.Results {
   public class AssemblyDriftIndex : IAssemblyQuantity<IDrift<double>> {
     public IDictionary<double, IDrift<double>> Results { get; private set; } = new Dictionary<double, IDrift<double>>();
+    public IList<string> Storeys { get; private set; } = new List<string>();
 
     internal AssemblyDriftIndex(ReadOnlyCollection<AssemblyDriftIndicesResult> result) {
       for (int i = 0; i < result.Count; i++) {
         Results.Add(result[i].Position, new DriftIndex(result[i]));
+        Storeys.Add(result[i].Storey);
       }
     }
   }
--- GsaGH/Parameters/5_Results/6_Quantities/AssemblyInternalForce.cs
+++ GsaGH/Parameters/5_Results/6_Quantities/AssemblyInternalForce.cs
@@ -5,10 +5,12 @@ using GsaAPI;
 namespace GsaGH.Parameters.Results {
   public class AssemblyInternalForce : IAssemblyQuantity<IInternalForce> {
     public IDictionary<double, IInternalForce> Results { get; private set; } = new Dictionary<double, IInternalForce>();
+    public IList<string> Storeys { get; private set; } = new List<string>();
 
     internal AssemblyInternalForce(ReadOnlyCollection<AssemblyResult> result) {
       for (int i = 0; i < result.Count; i++) {
         Results.Add(result[i].Position, new InternalForce(result[i]));
+        Storeys.Add(result[i].Storey);
       }
     }
   }
