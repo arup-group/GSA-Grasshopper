eebbcddbd600186dd83616d81998f52225fe4b9f
Tilman Reinhardt <tilman.reinhardt@arup.com>
2024-02-15 15:38:51 +0100

    GSAGH-455 PythagoreanTriple

--- GsaGH/Parameters/5_Results/6_Quantities/InternalForce.cs
+++ GsaGH/Parameters/5_Results/6_Quantities/InternalForce.cs
@@ -8,32 +8,32 @@ namespace GsaGH.Parameters.Results {
     public Force X { get; internal set; }
     public Force Y { get; internal set; }
     public Force Z { get; internal set; }
-    public Force Xyz { get; internal set; }
+    public Force Xyz { get; internal set; } // here Yz!
     public Moment Xx { get; internal set; }
     public Moment Yy { get; internal set; }
     public Moment Zz { get; internal set; }
-    public Moment Xxyyzz { get; internal set; }
+    public Moment Xxyyzz { get; internal set; } // here Yyzz!
 
     internal InternalForce(Double6 result) {
       X = new Force(result.X, ForceUnit.Newton);
       Y = new Force(result.Y, ForceUnit.Newton);
       Z = new Force(result.Z, ForceUnit.Newton);
-      Xyz = QuantityUtility.PythagoreanQuadruple(X,Y, Z);
+      Xyz = QuantityUtility.PythagoreanTriple(Y, Z);
       Xx = new Moment(result.XX, MomentUnit.NewtonMeter);
       Yy = new Moment(result.YY, MomentUnit.NewtonMeter);
       Zz = new Moment(result.ZZ, MomentUnit.NewtonMeter);
-      Xxyyzz = QuantityUtility.PythagoreanQuadruple(Xx, Yy, Zz);
+      Xxyyzz = QuantityUtility.PythagoreanTriple(Yy, Zz);
     }
 
     internal InternalForce(AssemblyResult result) {
       X = new Force(result.Values.X, ForceUnit.Newton);
       Y = new Force(result.Values.Y, ForceUnit.Newton);
       Z = new Force(result.Values.Z, ForceUnit.Newton);
-      Xyz = QuantityUtility.PythagoreanQuadruple(X, Y, Z);
+      Xyz = QuantityUtility.PythagoreanTriple(Y, Z);
       Xx = new Moment(result.Values.XX, MomentUnit.NewtonMeter);
       Yy = new Moment(result.Values.YY, MomentUnit.NewtonMeter);
       Zz = new Moment(result.Values.ZZ, MomentUnit.NewtonMeter);
-      Xxyyzz = QuantityUtility.PythagoreanQuadruple(Xx, Yy, Zz);
+      Xxyyzz = QuantityUtility.PythagoreanTriple(Yy, Zz);
     }
   }
 }
