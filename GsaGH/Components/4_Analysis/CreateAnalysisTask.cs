b9cb49ce137bb3c5a900c370d0502fb16f8702a0
Tilman Reinhardt <tilman.reinhardt@arup.com>
2024-05-30 15:11:59 +0200

    GSAGH-498 UpdateParameters()

--- GsaGH/Components/4_Analysis/CreateAnalysisTask.cs
+++ GsaGH/Components/4_Analysis/CreateAnalysisTask.cs
@@ -470,6 +470,7 @@ namespace GsaGH.Components {
 
     protected override void UpdateUIFromSelectedItems() {
       _type = _solverTypes[_selectedItems[0]];
+      UpdateParameters();
 
       base.UpdateUIFromSelectedItems();
     }
