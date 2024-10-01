341f6bd5983cfae9a39c4066ad917af4dd443ee5
Stamatios Psarras <psarras.st@gmail.com>
2024-10-01 12:14:57 +0100

    GSAGH 525: implement load panel for 2D member (#694)
    * feat: prop2d with Load Panels sets the Analysis Type of a member to Load Panel
    
    * feat: do not set the ApiMember.Type2D when the ApiMember is null
    
    * GSAGH 525: implement load panel for edit 2D member
    
    * GSAGH 525: update mapping to include load panel
    
    * feat: add on the description the option for Load Panel
    
    * feat: also optimised member type and added a test
    
    ---------
    
    Co-authored-by: Sandeep Kumar <sandeep.kumar@arup.com>
    Co-authored-by: SandeepArup <61278421+SandeepArup@users.noreply.github.com>

---
+++ GsaGH/Helpers/TextFormatHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GsaGH.Helpers {
+  public static class TextFormatHelper {
+
+    public static string FormatNumberedList(Dictionary<int, string> elementTypeMapping) {
+      return string.Join(Environment.NewLine,
+        elementTypeMapping.Select(x => $"{x.Key}: {x.Value}"));
+    }
+  }
+}
