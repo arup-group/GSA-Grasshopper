75aa118cac15ca422ea9b6dd85739bc148ef6ba3
DominikaLos <dominika.los@arup.com>
2024-10-03 17:03:22 +0200

    feat: add new file

---
+++ GsaGH/Helpers/GH/InputAttributes.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+using Grasshopper.Kernel;
+
+namespace GsaGH.Helpers.GH {
+  public struct InputAttributes {
+    public Type ParamType;
+    public string NickName;
+    public string Name;
+    public string Description;
+    public GH_ParamAccess Access;
+    public bool Optional;
+  }
+}
