b00b657a34ecfdf5872600e31c5266b273789f6d
Sandeep Kumar <sandeep.kumar@arup.com>
2024-10-05 12:28:10 +0530

    GSAGH-530 - morph test for 2d element

--- GsaGHTests/2_GooWrappers/GsaElement2dGooTests.cs
+++ GsaGHTests/2_GooWrappers/GsaElement2dGooTests.cs
@@ -1,12 +1,16 @@
 ﻿using Grasshopper.Kernel.Types;
 
+using GsaAPI;
+
 using GsaGH.Components;
 using GsaGH.Parameters;
 
 using GsaGHTests.Components.Geometry;
 using GsaGHTests.Helpers;
+using GsaGHTests.Parameters;
 
 using Rhino.Geometry;
+using Rhino.Geometry.Morphs;
 
 using Xunit;
 
@@ -49,6 +53,28 @@ namespace GsaGHTests.GooWrappers {
       var curve = new GH_Curve();
       Assert.True(output.CastTo(ref curve));
       Assert.True(curve.IsValid);
+    }
+
+    [Fact]
+    public void MorphMeshTest() {
+      var comp = (Section3dPreviewComponent)CreateElement2dTests.ComponentMother();
+      var output = (GsaElement2dGoo)ComponentTestHelper.GetOutput(comp);
+      var morph = new StretchSpaceMorph(new Point3d(0, 0, 0), new Point3d(2, 0, 0), 3);
+      var morphed = (GsaElement2dGoo)output.Morph(morph);
+      Assert.NotNull(morphed);
+      Assert.Equal(1.5, morphed.Value.Mesh.Vertices[1].X);
+    }
+
+    [Fact]
+    public void MorphCurveTest() {
+      var comp = (Section3dPreviewComponent)CreateElement2dTests.ComponentMotherLoadPanel();
+      var output = (GsaElement2dGoo)ComponentTestHelper.GetOutput(comp);
+      var morph = new StretchSpaceMorph(new Point3d(0, 0, 0), new Point3d(2, 0, 0), 3);
+      var morphed = (GsaElement2dGoo)output.Morph(morph);
+      var polyline = new Rhino.Geometry.Polyline();
+      morphed.Value.Curve.TryGetPolyline(out polyline);
+      Assert.NotNull(morphed);
+      Assert.Equal(1.5, morphed.Value.Curve.PointAt(1).X);
     }
   }
 }
