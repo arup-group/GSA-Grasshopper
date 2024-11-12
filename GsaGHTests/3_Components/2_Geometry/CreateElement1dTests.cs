a0f5b398665a142d24e6a73b42b868258db834aa
DominikaLos <dominika.los@arup.com>
2024-11-12 11:33:52 +0100

    bugfix: create element1d tests cleanup

--- GsaGHTests/3_Components/2_Geometry/CreateElement1dTests.cs
+++ GsaGHTests/3_Components/2_Geometry/CreateElement1dTests.cs
@@ -16,45 +16,92 @@ using Xunit;
 namespace GsaGHTests.Components.Geometry {
   [Collection("GrasshopperFixture collection")]
   public class CreateElement1dTests {
+    private readonly GsaElement1dGoo _element1dGoo;
+    private GsaElement1dGoo _element1dGooFromSection3d;
+
+    public CreateElement1dTests() {
+      GH_OasysComponent comp = ComponentMother();
+      _element1dGoo = (GsaElement1dGoo)ComponentTestHelper.GetOutput(comp);
+    }
 
     public static GH_OasysComponent ComponentMother() {
       var comp = new Create1dElement();
       comp.CreateAttributes();
 
-      ComponentTestHelper.SetInput(
-        comp, new LineCurve(new Point3d(0, -1, 0), new Point3d(7, 3, 1)), 0);
+      ComponentTestHelper.SetInput(comp, new LineCurve(new Point3d(0, -1, 0), new Point3d(7, 3, 1)), 0);
       ComponentTestHelper.SetInput(comp, "STD CH(ft) 1 2 3 4", 1);
 
       return comp;
     }
 
-    [Fact]
-    public void CreateComponentTest() {
-      GH_OasysComponent comp = ComponentMother();
+    private void SetElement1dFromSection3d() {
+      if (_element1dGooFromSection3d != null) {
+        return;
+      }
 
-      var output = (GsaElement1dGoo)ComponentTestHelper.GetOutput(comp);
-      Assert.Equal(0, output.Value.Line.PointAtStart.X);
-      Assert.Equal(-1, output.Value.Line.PointAtStart.Y);
-      Assert.Equal(0, output.Value.Line.PointAtStart.Z);
-      Assert.Equal(7, output.Value.Line.PointAtEnd.X);
-      Assert.Equal(3, output.Value.Line.PointAtEnd.Y);
-      Assert.Equal(1, output.Value.Line.PointAtEnd.Z);
-      Assert.Equal("STD CH(ft) 1 2 3 4", output.Value.Section.ApiSection.Profile);
-      Assert.Equal(1, output.Value.ApiElement.Group);
+      var section3dPreview = (Section3dPreviewComponent)ComponentMother();
+      section3dPreview.Preview3dSection = true;
+      section3dPreview.ExpireSolution(true);
+      _element1dGooFromSection3d = (GsaElement1dGoo)ComponentTestHelper.GetOutput(section3dPreview);
     }
 
     [Fact]
-    public void CreateComponentWithSection3dPreviewTest() {
-      var comp = (Section3dPreviewComponent)ComponentMother();
-      comp.Preview3dSection = true;
-      comp.ExpireSolution(true);
+    public void ComponentShouldReturn0AsPointAtStartXValue() {
+      Assert.Equal(0, _element1dGoo.Value.Line.PointAtStart.X);
+    }
 
-      var output = (GsaElement1dGoo)ComponentTestHelper.GetOutput(comp);
-      Assert.Equal(16, output.Value.Section3dPreview.Mesh.Vertices.Count);
-      Assert.Equal(24, output.Value.Section3dPreview.Outlines.Count());
+    [Fact]
+    public void ComponentShouldReturnMinus1AsPointAtStartYValue() {
+      Assert.Equal(-1, _element1dGoo.Value.Line.PointAtStart.Y);
+    }
 
+    [Fact]
+    public void ComponentShouldReturn0AsPointAtStartZValue() {
+      Assert.Equal(0, _element1dGoo.Value.Line.PointAtStart.Z);
+    }
+
+    [Fact]
+    public void ComponentShouldReturn7AsPointAtEndXValue() {
+      Assert.Equal(7, _element1dGoo.Value.Line.PointAtEnd.X);
+    }
+
+    [Fact]
+    public void ComponentShouldReturn3AsPointAtEndYValue() {
+      Assert.Equal(3, _element1dGoo.Value.Line.PointAtEnd.Y);
+    }
+
+    [Fact]
+    public void ComponentShouldReturn1AsPointAtEndZValue() {
+      Assert.Equal(1, _element1dGoo.Value.Line.PointAtEnd.Z);
+    }
+
+    [Fact]
+    public void ComponentShouldReturnCorrectProfile() {
+      Assert.Equal("STD CH(ft) 1 2 3 4", _element1dGoo.Value.Section.ApiSection.Profile);
+    }
+
+    [Fact]
+    public void ComponentShouldReturnDefaultGroupValue() {
+      Assert.Equal(1, _element1dGoo.Value.ApiElement.Group);
+    }
+
+    [Fact]
+    public void CreateComponentWithSection3dPreviewShouldReturn16Vertices() {
+      SetElement1dFromSection3d();
+      Assert.Equal(16, _element1dGooFromSection3d.Value.Section3dPreview.Mesh.Vertices.Count);
+    }
+
+    [Fact]
+    public void CreateComponentWithSection3dPreviewShouldReturn24Outlines() {
+      SetElement1dFromSection3d();
+      Assert.Equal(24, _element1dGooFromSection3d.Value.Section3dPreview.Outlines.Count());
+    }
+
+    [Fact]
+    public void CreateComponentWithSection3dPreviewShouldCastToMesh() {
+      SetElement1dFromSection3d();
       var mesh = new GH_Mesh();
-      Assert.True(output.CastTo(ref mesh));
+      Assert.True(_element1dGooFromSection3d.CastTo(ref mesh));
     }
   }
 }
