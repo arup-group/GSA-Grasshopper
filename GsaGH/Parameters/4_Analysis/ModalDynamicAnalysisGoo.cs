841f793113c33aa592d6a657edf7d1f02ce9ed56
Sandeep Kumar <sandeep.kumar@arup.com>
2024-11-20 18:48:42 +0530

    GSAGH-499: Add modal analysis task

---
+++ GsaGH/Parameters/4_Analysis/ModalDynamicAnalysisGoo.cs
@@ -0,0 +1,33 @@
+﻿using System.Drawing;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+
+using GsaGH.Helpers.Graphics;
+
+using OasysGH;
+using OasysGH.Parameters;
+using OasysGH.Units;
+
+using Rhino.Display;
+using Rhino.Geometry;
+
+using Line = Rhino.Geometry.Line;
+
+namespace GsaGH.Parameters {
+  /// <summary>
+  ///   Goo wrapper class, makes sure <see cref="GsaEffectiveLengthOptions" /> can be used in Grasshopper.
+  /// </summary>
+  public class GsaModalDynamicAnalysisGoo : GH_OasysGoo<GsaModalDynamicAnalysis> {
+    public static string Description => "GSA modal dynamic analysis parameters";
+    public static string Name => "Modal Dynamic Parameter";
+    public static string NickName => "Mdp";
+    public override OasysPluginInfo PluginInfo => GsaGH.PluginInfo.Instance;
+
+    public GsaModalDynamicAnalysisGoo(GsaModalDynamicAnalysis item) : base(item) { }
+
+    public override IGH_Goo Duplicate() {
+      return new GsaModalDynamicAnalysisGoo(Value);
+    }
+  }
+}
---
+++ GsaGH/Parameters/4_Analysis/ModalDynamicAnalysisParameter.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Drawing;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Types;
+
+using GsaGH.Helpers.GH;
+using GsaGH.Helpers.Graphics;
+using GsaGH.Properties;
+
+using OasysGH;
+using OasysGH.Parameters;
+using OasysGH.Units;
+
+namespace GsaGH.Parameters {
+  /// <summary>
+  ///   This class provides a parameter interface for the <see cref="GsaModalDynamicAnalysisGoo" /> type.
+  /// </summary>
+  public class GsaModalDynamicAnalysisParameter : GH_OasysPersistentParam<GsaModalDynamicAnalysisGoo> {
+    public override Guid ComponentGuid => new Guid("6b99a192-bdbd-41bf-8efa-1bc146d3d224");
+    public override GH_Exposure Exposure => GH_Exposure.quinary | GH_Exposure.obscure;
+    public override string InstanceDescription => GsaModalDynamicAnalysisGoo.Name;
+    public override string TypeName => GsaModalDynamicAnalysisGoo.Name;
+    protected override Bitmap Icon => Resources.AnalysisCaseParam;
+
+    public GsaModalDynamicAnalysisParameter() : base(new GH_InstanceDescription(
+      GsaModalDynamicAnalysisGoo.Name, GsaModalDynamicAnalysisGoo.NickName,
+      GsaModalDynamicAnalysisGoo.Description,
+      CategoryName.Name(), SubCategoryName.Cat9())) { }
+
+    protected override GsaModalDynamicAnalysisGoo PreferredCast(object data) {
+      switch (data) {
+        case GsaAnalysisTaskGoo analysisTask:
+          return new GsaModalDynamicAnalysisGoo(new GsaModalDynamicAnalysis(analysisTask.Value.ApiTask));
+        case GsaModalDynamicAnalysisGoo dynamicAnalysis:
+          return new GsaModalDynamicAnalysisGoo(dynamicAnalysis.Value);
+        default:
+          this.AddRuntimeError($"Data conversion failed from {data.GetTypeName()} to ModalDynamicAnalysis parameter");
+          return new GsaModalDynamicAnalysisGoo(null);
+      }
+    }
+  }
+}
