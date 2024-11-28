2e4f79151a90ff425657ad00e11f607e13dd9d0d
DominikaLos <dominika.los@arup.com>
2024-11-28 09:58:26 +0100

    bugfix: renaming Legend variables

--- GsaGH/Helpers/GH/Legend.cs
+++ GsaGH/Helpers/GH/Legend.cs
@@ -5,28 +5,28 @@ using System.Linq;
 namespace GsaGH.Helpers.GH {
   public class Legend {
     public List<string> Values { get; set; } = new List<string>();
-    public List<int> ValuesY { get; set; } = new List<int>();
-    public bool ShowLegend { get; set; } = true;
+    public List<int> ValuesPositionY { get; set; } = new List<int>();
+    public bool Visible { get; set; } = true;
     public Bitmap Bitmap { get; set; } = null;
     public double Scale { get; set; } = 1.0;
 
     public Legend() { }
 
-    public Legend(List<string> values, List<int> valuesY, Bitmap bitmap, double scale, bool showLegend) {
+    public Legend(List<string> values, List<int> valuesPositionY, Bitmap bitmap, double scale, bool visible) {
       Values = values;
-      ValuesY = valuesY;
-      ShowLegend = showLegend;
+      ValuesPositionY = valuesPositionY;
+      Visible = visible;
       Bitmap = bitmap;
       Scale = scale;
     }
 
     public bool IsDisplayable() {
-      return Values.Any() && ShowLegend;
+      return Values.Any() && Visible;
     }
 
     public bool ToggleShowLegend() {
-      ShowLegend = !ShowLegend;
-      return ShowLegend;
+      Visible = !Visible;
+      return Visible;
     }
 
     public Bitmap CreateNewBitmap(int widthScaleFactor, int heightScaleFactor) {
